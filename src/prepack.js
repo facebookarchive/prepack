b55d11c1efc8b500412e83edbfdc9f2d877a2af7
Chris Blappert <blappert@fb.com>
2017-03-29 15:48:22 -0700

    Moving things around so prepack 'run' can be included as a module

--- src/prepack.js
+++ src/prepack.js
@@ -8,14 +8,12 @@
  */
 
 /* @flow */
-
 import Serialiser from "./serialiser.js";
 import invariant from "./invariant.js";
-
 let fs        = require("fs");
 
-function run_internal(name: string, raw: string, map: string = "", compatibility?: "browser" | "jsc" = "browser", mathRandomSeed: void | string, outputFilename?: string, outputMap?: string) {
-  let serialised = new Serialiser({ partial: true, compatibility, mathRandomSeed }).init(name, raw, map, outputMap !== undefined);
+function run_internal(name: string, raw: string, map: string = "", compatibility?: "browser" | "jsc" = "browser", mathRandomSeed: void | string, outputFilename?: string, outputMap?: string, speculate: boolean = false) {
+  let serialised = new Serialiser({ partial: true, compatibility, mathRandomSeed }, speculate).init(name, raw, map, outputMap !== undefined);
   if (!serialised) {
     process.exit(1);
     invariant(false);
@@ -42,44 +40,15 @@ function run_internal(name: string, raw: string, map: string = "", compatibility
   }
 }
 
-let args = Array.from(process.argv);
-args.splice(0, 2);
-let inputFilename;
-let outputFilename;
-let compatibility;
-let mathRandomSeed;
-let inputMap;
-let ouputMap;
-while (args.length) {
-  let arg = args[0]; args.shift();
-  if (arg === "--out") {
-    arg = args[0]; args.shift();
-    outputFilename = arg;
-  } else if (arg === "--compatibility") {
-    arg = args[0]; args.shift();
-    if (arg !== "jsc") {
-      console.error(`Unsupported compatibility: ${arg}`);
-      process.exit(1);
-    } else {
-      compatibility = arg;
-    }
-  } else if (arg === "--mathRandomSeed") {
-    mathRandomSeed = args[0]; args.shift();
-  } else if (arg === "--srcmapIn") {
-    inputMap = args[0]; args.shift();
-  } else if (arg === "--srcmapOut") {
-    ouputMap = args[0]; args.shift();
-  } else if (arg === "--help") {
-    console.log("Usage: prepack.js [ --out output.js ] [ --compatibility jsc ] [ --mathRandomSeed seedvalue ] [ --srcmapIn inputMap ] [ --srcmapOut outputMap ] [ -- | input.js ]");
-  } else if (!arg.startsWith("--")) {
-    inputFilename = arg;
-  } else {
-    console.error(`Unknown option: ${arg}`);
-    process.exit(1);
-  }
-}
-
-export function run(inFn: string, compat?: "browser" | "jsc" = "browser", mathRandSeed: void | string, outFn?: string, outMap?: string) {
+export function run(inFn: string, compat?: "browser" | "jsc" = "browser", mathRandSeed: void | string, outFn?: string, inputMap?: string, outMap?: string, speculateOpt?: boolean) {
+  console.log("running args:")
+  console.log(inFn);
+  console.log(compat);
+  console.log(mathRandSeed);
+  console.log(outFn);
+  console.log(inputMap);
+  console.log(outMap);
+  console.log(speculateOpt);
   let input = fs.readFileSync(inFn, "utf8");
   let map = "";
   let mapFile = inputMap ? inputMap : inFn + ".map";
@@ -88,12 +57,5 @@ export function run(inFn: string, compat?: "browser" | "jsc" = "browser", mathRa
   } catch (_e) {
     console.log(`No sourcemap found at ${mapFile}.`);
   }
-  run_internal(inFn, input, map, compat, mathRandSeed, outFn, outMap);
-}
-
-if (!inputFilename) {
-  console.error("Missing input file.");
-  process.exit(1);
-} else {
-  run(inputFilename, compatibility, mathRandomSeed, outputFilename, ouputMap);
+  run_internal(inFn, input, map, compat, mathRandSeed, outFn, outMap, speculateOpt);
 }
---
+++ src/run_util.js
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+/* @flow */
+
+import Serialiser from "./serialiser.js";
+import invariant from "./invariant.js";
+import run from "./prepack.js";
+
+let fs        = require("fs");
+
+let oldwfs = fs.writeFileSync;
+fs.writeFileSync = (fn, txt) => { console.log(`writing ${fn}`); oldwfs(fn, txt); };
+
+let args = Array.from(process.argv);
+args.splice(0, 2);
+let inputFilename;
+let outputFilename;
+let compatibility;
+let mathRandomSeed;
+let inputMap;
+let ouputMap;
+let speculate = false;
+while (args.length) {
+  let arg = args[0]; args.shift();
+  console.log(arg);
+  if (arg === "--out") {
+    arg = args[0]; args.shift();
+    console.log(arg);
+    outputFilename = arg;
+  } else if (arg === "--compatibility") {
+    arg = args[0]; args.shift();
+    if (arg !== "jsc") {
+      console.error(`Unsupported compatibility: ${arg}`);
+      process.exit(1);
+    } else {
+      compatibility = arg;
+    }
+  } else if (arg === "--mathRandomSeed") {
+    mathRandomSeed = args[0]; args.shift();
+  } else if (arg === "--srcmapIn") {
+    inputMap = args[0]; args.shift();
+  } else if (arg === "--srcmapOut") {
+    ouputMap = args[0]; args.shift();
+  } else if (arg === "--speculate") {
+    speculate = true;
+  } else if (arg === "--help") {
+    console.log("Usage: prepack.js [ --out output.js ] [ --compatibility jsc ] [ --mathRandomSeed seedvalue ] [ --srcmapIn inputMap ] [ --srcmapOut outputMap ] [ -- | input.js ]");
+  } else if (!arg.startsWith("--")) {
+    inputFilename = arg;
+  } else {
+    console.error(`Unknown option: ${arg}`);
+    process.exit(1);
+  }
+}
+
+if (!inputFilename) {
+  console.error("Missing input file.");
+  process.exit(1);
+} else {
+  run(inputFilename, compatibility, mathRandomSeed, outputFilename, inputMap, ouputMap, speculate);
+}
