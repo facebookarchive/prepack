ea285f106863314fd6543933a9994f77ef9f840d
Nikolai Tillmann <nikolait@fb.com>
2017-04-04 20:44:50 -0700

    Adding test for __assumeDataProperty. Wrap code in the serialiser that may run user code into tryQuery (otherwise, it may crash the serialiser, and did so for the new test).

---
+++ test/serialiser/basic/AssumeDataProperty.js
@@ -0,0 +1,5 @@
+if (global.__assumeDataProperty) __assumeDataProperty(global, "o", undefined);
+if (global.__assumeDataProperty) __assumeDataProperty(global, "inspect", undefined);
+if (global.__makePartial) __makePartial(global);
+o = 42;
+inspect = function() { return o; }
