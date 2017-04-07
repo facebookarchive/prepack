53cae2c9a74ff7b1e42094bb0076997f80f974fe
Eli White <github@eli-white.com>
2017-04-07 14:38:05 -0700

    Redirect the console output into the error pane

--- js/repl-worker.js
+++ js/repl-worker.js
@@ -1,7 +1,18 @@
 self.importScripts('prepack.min.js');
 
 onmessage = function(e) {
+  var buffer = '';
+  var originError = console.error;
+  var originalWarn = console.warn;
+
+  function redirectOutput() {
+    buffer += Array.from(arguments).join(' ') + '\n';
+  }
+
   try {
+    console.error = redirectOutput;
+    console.warn = redirectOutput;
+
     var result = new prepack.default(
       {
         partial: true,
@@ -12,6 +23,9 @@ onmessage = function(e) {
 
     postMessage({type: 'success', data: result.code});
   } catch (err) {
-    postMessage({type: 'error', data: err.nativeStack});
+    postMessage({type: 'error', data: buffer || err.nativeStack});
+  } finally {
+    console.error = originalError;
+    console.warn = originalWarn;
   }
 };
