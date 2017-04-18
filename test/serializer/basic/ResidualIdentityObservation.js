01b4afccf807ab9e29a48ae4437e2eab4297c4c4
Nikolai Tillmann <nikolait@fb.com>
2017-04-18 16:40:48 -0700

    Captured variables that are referenced by residual functions and that possibly hold objects need to be treated as locations, not values that can be re-created on-the-fly, as the object identity may be observed.
    Added regression test.

---
+++ test/serializer/basic/ResidualIdentityObservation.js
@@ -0,0 +1,10 @@
+(function() {
+    var a = {x: 4};
+    function f() {
+        return a;
+    }
+    function g() {
+        return a;
+    }
+    inspect = function() { return f() === g(); }
+})();
