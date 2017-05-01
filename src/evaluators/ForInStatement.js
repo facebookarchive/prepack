22c0bad192d5e2fbdce77f7d199dc4f847e6ef17
Herman Venter <hermanv@fb.com>
2017-05-01 15:20:21 -0700

    for in loops over simple abstract objects (#478)

--- src/evaluators/ForInStatement.js
+++ src/evaluators/ForInStatement.js
@@ -10,12 +10,16 @@
 /* @flow */
 
 import type { Realm } from "../realm.js";
-import type { Value } from "../values/index.js";
 import type { LexicalEnvironment, Reference } from "../environment.js";
 import { BreakCompletion } from "../completions.js";
+import { TypesDomain, ValuesDomain } from "../domains/index.js";
+import { DeclarativeEnvironmentRecord } from "../environment.js";
 import { ForInOfHeadEvaluation, ForInOfBodyEvaluation } from "./ForOfStatement.js";
-import { BoundNames, UpdateEmpty } from "../methods/index.js";
-import type { BabelNodeForInStatement } from "babel-types";
+import { BoundNames, NewDeclarativeEnvironment, UpdateEmpty } from "../methods/index.js";
+import { AbstractValue, AbstractObjectValue, ObjectValue, StringValue, UndefinedValue, Value } from "../values/index.js";
+import type { BabelNodeForInStatement, BabelNodeStatement, BabelNodeVariableDeclaration } from "babel-types";
+import invariant from "../invariant.js";
+import * as t from "babel-types";
 
 // ECMA262 13.7.5.11
 export default function (ast: BabelNodeForInStatement, strictCode: boolean, env: LexicalEnvironment, realm: Realm, labelSet: ?Array<string>): Value | Reference {
@@ -27,11 +31,16 @@ export default function (ast: BabelNodeForInStatement, strictCode: boolean, env:
         // 1. Let keyResult be ? ForIn/OfHeadEvaluation(« », Expression, enumerate).
         let keyResult = ForInOfHeadEvaluation(realm, env, [], right, "enumerate", strictCode);
 
+        if (keyResult instanceof AbstractObjectValue) {
+          return emitResidualLoopIfSafe(ast, strictCode, env, realm, left, keyResult, body);
+        }
+
         // 2. Return ? ForIn/OfBodyEvaluation(ForBinding, Statement, keyResult, varBinding, labelSet).
         return ForInOfBodyEvaluation(realm, env, left.declarations[0].id, body, keyResult, "varBinding", labelSet, strictCode);
       } else { // for (ForDeclaration in Expression) Statement
         // 1. Let keyResult be the result of performing ? ForIn/OfHeadEvaluation(BoundNames of ForDeclaration, Expression, enumerate).
         let keyResult = ForInOfHeadEvaluation(realm, env, BoundNames(realm, left), right, "enumerate", strictCode);
+        keyResult = keyResult.throwIfNotConcreteObject();
 
         // 2. Return ? ForIn/OfBodyEvaluation(ForDeclaration, Statement, keyResult, lexicalBinding, labelSet).
         return ForInOfBodyEvaluation(realm, env, left, body, keyResult, "lexicalBinding", labelSet, strictCode);
@@ -39,6 +48,7 @@ export default function (ast: BabelNodeForInStatement, strictCode: boolean, env:
     } else { // for (LeftHandSideExpression in Expression) Statement
       // 1. Let keyResult be ? ForIn/OfHeadEvaluation(« », Expression, enumerate).
       let keyResult = ForInOfHeadEvaluation(realm, env, [], right, "enumerate", strictCode);
+      keyResult = keyResult.throwIfNotConcreteObject();
 
       // 2. Return ? ForIn/OfBodyEvaluation(LeftHandSideExpression, Statement, keyResult, assignment, labelSet).
       return ForInOfBodyEvaluation(realm, env, left, body, keyResult, "assignment", labelSet, strictCode);
@@ -46,8 +56,82 @@ export default function (ast: BabelNodeForInStatement, strictCode: boolean, env:
   } catch (e) {
     if (e instanceof BreakCompletion) {
       if (!e.target)
-       return (UpdateEmpty(realm, e, realm.intrinsics.undefined): any).value;
+        return (UpdateEmpty(realm, e, realm.intrinsics.undefined): any).value;
     }
     throw e;
   }
+}
+
+function emitResidualLoopIfSafe(ast: BabelNodeForInStatement, strictCode: boolean, env: LexicalEnvironment, realm: Realm,
+    lh: BabelNodeVariableDeclaration, ob: AbstractObjectValue, body: BabelNodeStatement) {
+  let oldEnv = realm.getRunningContext().lexicalEnvironment;
+  let blockEnv = NewDeclarativeEnvironment(realm, oldEnv);
+  realm.getRunningContext().lexicalEnvironment = blockEnv;
+  try {
+    let envRec = blockEnv.environmentRecord;
+    invariant(envRec instanceof DeclarativeEnvironmentRecord, "expected declarative environment record");
+    let absStr = realm.createAbstract(
+      new TypesDomain(StringValue), ValuesDomain.topVal, [], t.stringLiteral("never used"));
+    let boundName;
+    for (let n of BoundNames(realm, lh)) {
+      if (boundName !== undefined) {
+        ob.throwIfNotConcreteObject();
+        return realm.intrinsics.undefined;
+      }
+      boundName = t.identifier(n);
+      envRec.CreateMutableBinding(n, false);
+      envRec.InitializeBinding(n, absStr);
+    }
+    let [compl, gen, bindings, properties, createdObj] =
+      realm.partially_evaluate_node(body, strictCode, blockEnv);
+    if (compl instanceof Value && gen.body.length === 0 && bindings.size === 0 &&
+        properties.size === 1 && createdObj.size === 0) {
+      let targetObject;
+      let sourceObject;
+      properties.forEach((desc, key, map) => {
+        if (key.object.unknownProperty === key) {
+          targetObject = key.object;
+          invariant(desc !== undefined);
+          let sourceValue = desc.value;
+          if (sourceValue instanceof AbstractValue) {
+            let cond = sourceValue.args[0];
+            if (cond instanceof AbstractValue && cond.kind === "template for property name condition") {
+              if (sourceValue.args[2] instanceof UndefinedValue) {
+                let mem = sourceValue.args[1];
+                if (mem instanceof AbstractValue && mem.kind === "sentinel member expression") {
+                  if (mem.args[0] instanceof ObjectValue && mem.args[1] === absStr) {
+                    sourceObject = mem.args[0];
+                  }
+                }
+              }
+            }
+          }
+        }
+      });
+      if (targetObject !== undefined && sourceObject !== undefined) {
+        let oe = ob.values.getElements();
+        if (oe.size !== 1) ob.throwIfNotConcreteObject();
+        let o; for (let co of oe) o = co; invariant(o !== undefined);
+        let generator = realm.generator; invariant(generator !== undefined);
+        generator.body.push({
+          // duplicate args to ensure refcount > 1
+          args: [o, targetObject, sourceObject, ob, targetObject, sourceObject],
+          buildNode: ([obj, tgt, src, obj1, tgt1, src1]) => {
+            invariant(boundName !== undefined);
+            return t.forInStatement(lh, obj,
+              t.blockStatement([t.expressionStatement(t.assignmentExpression("=",
+              t.memberExpression(tgt, boundName, true),
+              t.memberExpression(src, boundName, true)))]));
+          },
+        });
+        return realm.intrinsics.undefined;
+      }
+    }
+  } finally {
+    // 6. Set the running execution context's LexicalEnvironment to oldEnv.
+    realm.getRunningContext().lexicalEnvironment = oldEnv;
+  }
+
+  ob.throwIfNotConcreteObject();
+  return realm.intrinsics.undefined;
 }
---
+++ test/serializer/abstract/ObjectAssign3.js
@@ -0,0 +1,18 @@
+let ob = global.__abstract ? __abstract({}, "({p: 1})") : {p: 1};
+if (global.__makeSimple) __makeSimple(ob);
+
+Object.assign = function(target, sources) {
+  for (let nextIndex = 1; nextIndex < arguments.length; nextIndex++) {
+    let nextSource = arguments[nextIndex];
+    if (nextSource == null) continue;
+    for (var key in nextSource) {
+      target[key] = nextSource[key];
+    }
+  }
+
+  return target;
+};
+
+z = Object.assign({}, ob);
+
+inspect = function() { return z.p; }
