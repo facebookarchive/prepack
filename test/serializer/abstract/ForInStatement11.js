272e49c901aa624bcebb1c48e80533173b11e1bd
Herman Venter <hermanv@fb.com>
2017-05-01 19:01:50 -0700

    Add negative test cases for for-in loop (#493)

---
+++ test/serializer/abstract/ForInStatement11.js
@@ -0,0 +1,9 @@
+// throws introspection error
+
+let ob = global.__abstract ? __abstract({ x: 1 }, "({ x: 1 })") : { x: 1 };
+if (global.__makeSimple) __makeSimple(ob);
+
+let tgt = {};
+for (var p in ob) {
+  console.log(p);
+}
