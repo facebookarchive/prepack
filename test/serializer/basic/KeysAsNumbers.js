c196f08339244e783c3c8f115712322ce1037e41
Nikolai Tillmann <nikolait@fb.com>
2017-05-02 16:38:09 -0700

    Fix handling of integer keys. Added regression test.

---
+++ test/serializer/basic/KeysAsNumbers.js
@@ -0,0 +1,13 @@
+// does not contain:"99
+(function () {
+  let a = { 991: -5, "a": 1 };
+  Object.defineProperty(a, 992, { configurable: false, enumerable: false, writable: true, value: -10 });
+  Object.defineProperty(a, "b", { configurable: false, enumerable: false, writable: true, value: 3 });
+  let sum = function(a) {
+      let res = 0;
+      for (let i=0; i<a.length; i++) res += a[i];
+      return res;
+  }
+  inspect = function() { 
+      return sum(a) === -11; };
+})();
