e98a51789452800e17ee8118728c96ec917f0fc2
Sebastian Markbage <sema@fb.com>
2017-05-05 14:32:08 -0700

    Use an existing property name to test throwing in global getters
    This works around in a bug in Node.js 7.x.x where added properties need to
    be copied over onto the original "sandbox" created by the VM module in the
    test-runner. That is done in native code which is not resilient to this
    getter throwing which causes a crash.
    
    By using an existing property name, that path is skipped.

--- test/serializer/basic/GlobalPropertyGetter.js
+++ test/serializer/basic/GlobalPropertyGetter.js
@@ -1,12 +1,14 @@
-Object.defineProperty(global, "p", { 
-  configurable: true, 
-  enumerable: true, 
+// We use the name "console" because it already exists.
+// Adding a non-existing property will break our test-runner in Node 7.x.x.
+Object.defineProperty(global, "console", {
+  configurable: true,
+  enumerable: true,
   get: function () { throw new Error("42"); }
 });
 
-inspect = function() { 
+inspect = function() {
   try {
-    return p; 
+    return console;
   } catch(e) {
     return e.message;
   }
