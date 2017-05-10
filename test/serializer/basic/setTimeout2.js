c5d8329ee5dfc5fc950ced2ca24d727cbc93797f
Nikolai Tillmann <nikolait@fb.com>
2017-05-10 10:52:43 -0700

    Also implement clearTimeout and clearInterval, so that tests can terminate.

--- test/serializer/basic/setTimeout2.js
+++
@@ -1,7 +0,0 @@
-// does contain:keep me
-(function() {
-    let f = function() { /* keep me */ };
-    setTimeout(f, 1000);
-    setInterval(f, 1000);
-    inspect = function() { return true; }
-})();
