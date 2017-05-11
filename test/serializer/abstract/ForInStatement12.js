7514c7abb62883ae2ea6ed46a1a3d9e40832508f
Nikolai Tillmann <nikolai_tillmann@hotmail.com>
2017-05-10 17:09:20 -0700

    Fixing deficiency in abstract For-In treatment. (#631)
    * Fixing deficiency in abstract For-In treatment.
    Identified a number of possible issues, leaving behind some TODOs.
    Adding test created by Chris.
    
    * Fixing lint/flow issues

---
+++ test/serializer/abstract/ForInStatement12.js
@@ -0,0 +1,19 @@
+let tmp = { w: undefined, nest: { x: 1 } };
+let str = "({ w: undefined, nest: { x: 1 } })";
+let ob = global.__abstract ? __abstract(tmp, str) : tmp;
+
+let tgt = {};
+
+ob = JSON.parse(JSON.stringify(ob));
+let loc = ob.nest;
+ob.w = {
+    a: loc.x / loc.x,
+    b: loc.x + loc.x
+};
+for (var p in ob) {
+    tgt[p] = ob[p];
+}
+let final_p1 = tgt.w;
+let finalv = final_p1.a;
+
+inspect = function() { return finalv +  tgt.w.a + tgt.w.b; }
