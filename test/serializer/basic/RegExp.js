ec7f41f7d9b9dc8d5b349f0f252b53835d267c59
Herman Venter <hermanv@fb.com>
2017-05-15 17:22:48 -0700

    Make serializer smarter about regular expression literals (#645)

--- test/serializer/basic/RegExp.js
+++ test/serializer/basic/RegExp.js
@@ -1,3 +1,11 @@
 regexp = /Facebook/i;
+regexp2 = /books/;
+regexp3 = new RegExp("/");
 
-inspect = function() { return "Facebook is cool".match(regexp).index; }
+regexp2.lastIndex = 8;
+i = "but books are books".match(regexp2);
+j = regexp2.lastIndex;
+
+k = "and /books/ are books too".match(regexp3);
+
+inspect = function() { return "" + i + j + k + "Facebook is cool".match(regexp).index; }
