9cec90d0ab1e18f6705ac5c0f7c1b776526116b8
Herman Venter <hermanv@fb.com>
2017-06-01 15:21:02 -0700

    Partial evaluator for block

--- src/partial-evaluators/BlockStatement.js
+++ src/partial-evaluators/BlockStatement.js
@@ -13,13 +13,46 @@ import type { BabelNodeBlockStatement, BabelNodeStatement } from "babel-types";
 import type { Realm } from "../realm.js";
 import type { LexicalEnvironment } from "../environment.js";
 
-import { AbruptCompletion } from "../completions.js";
-import { Value } from "../values/index.js";
+import { Completion, NormalCompletion } from "../completions.js";
+import { EmptyValue, StringValue, Value } from "../values/index.js";
+import { BlockDeclarationInstantiation, NewDeclarativeEnvironment, PartiallyEvaluateStatements } from "../methods/index.js";
+
+import invariant from "../invariant.js";
+import * as t from "babel-types";
 
 // ECMA262 13.2.13
 export default function (
   ast: BabelNodeBlockStatement, strictCode: boolean, env: LexicalEnvironment, realm: Realm
-): [AbruptCompletion | Value, BabelNodeBlockStatement, Array<BabelNodeStatement>] {
-  let result = env.evaluateCompletionDeref(ast, strictCode);
-  return [result, ast, []];
+): [Completion | Value, BabelNodeStatement, Array<BabelNodeStatement>] {
+  // 1. Let oldEnv be the running execution context's LexicalEnvironment.
+  let oldEnv = realm.getRunningContext().lexicalEnvironment;
+
+  // 2. Let blockEnv be NewDeclarativeEnvironment(oldEnv).
+  let blockEnv = NewDeclarativeEnvironment(realm, oldEnv);
+
+  // 3. Perform BlockDeclarationInstantiation(StatementList, blockEnv).
+  BlockDeclarationInstantiation(realm, strictCode, ast.body, blockEnv);
+
+  // 4. Set the running execution context's LexicalEnvironment to blockEnv.
+  realm.getRunningContext().lexicalEnvironment = blockEnv;
+
+  try {
+    // 5. Let blockValue be the result of evaluating StatementList.
+    let blockValue: void | NormalCompletion | Value;
+
+    if (ast.directives) {
+      for (let directive of (ast.directives)) {
+        blockValue = new StringValue(realm, directive.value.value);
+      }
+    }
+
+    let [res, bAst] = PartiallyEvaluateStatements(ast.body, blockValue, strictCode, blockEnv, realm);
+    invariant(bAst.length > 0 || res instanceof EmptyValue);
+    if (bAst.length === 0) return [res, t.emptyStatement(), []];
+    let rAst = t.blockStatement(bAst, ast.directives);
+    return [res, rAst, []];
+  } finally {
+    // 6. Set the running execution context's LexicalEnvironment to oldEnv.
+    realm.getRunningContext().lexicalEnvironment = oldEnv;
+  }
 }
