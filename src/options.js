a9d4b1c736d77b030877bd41a326dace19ea872e
Herman Venter <hermanv@fb.com>
2017-06-05 16:55:23 -0700

    Update options.js

--- src/options.js
+++ src/options.js
@@ -33,7 +33,7 @@ export type Options = {|
   strictlyMonotonicDateNow?: boolean,
 |};
 
-export const defaultOptions = { };
+export const defaultOptions = {};
 
 export function getRealmOptions({
   compatibility = "browser",
