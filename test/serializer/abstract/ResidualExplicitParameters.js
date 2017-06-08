18ed6c906f94764bd9e0a0e76563f126128b3ce9
Kishore B. Rao <kishbrao@fb.com>
2017-06-08 15:09:55 -0700

    Added test that catches the explicit parameter bug/issue

---
+++ test/serializer/abstract/ResidualExplicitParameters.js
@@ -0,0 +1,5 @@
+(function() {
+  let x = 42;
+  let y = global.__residual ? __residual("number", function () { return x; }) : 42; // this variant takes no further arguments
+  inspect = function() { return y; }
+})();
