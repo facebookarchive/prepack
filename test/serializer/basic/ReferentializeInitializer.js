cc0f6ff156cf493b3992718e7346bd4f570f9c04
Nikolai Tillmann <nikolait@fb.com>
2017-06-19 19:54:44 -0700

    Adding regression test for feature interaction between referentialization (of variables) and delayed initialization (of values).

---
+++ test/serializer/basic/ReferentializeInitializer.js
@@ -0,0 +1,10 @@
+(function() {
+  function f() {
+    let obj = { count: 0 };
+    return function(reset) { if (reset) obj = { count: 0 }; return obj.count++; }
+  }
+  g = f();
+  inspect = function() {
+    return "" + g(false) + g(true) + g(false);
+  }
+})();
