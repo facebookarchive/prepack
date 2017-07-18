6faf389083c7a9a1d101c92f43db6ac5a6b1f6f4
Alex Dvornikov <advornikov@fb.com>
2017-07-18 12:12:24 -0700

    Fix property ordering for delayed values
    Summary:
    Resolves object property insertion order issue described here: https://github.com/facebook/prepack/issues/730
    Closes https://github.com/facebook/prepack/pull/808
    
    Differential Revision: D5444833
    
    Pulled By: cblappert
    
    fbshipit-source-id: 0e27a5d8ae151cbc1169af6472062383b7a4c1c1

---
+++ test/serializer/basic/DelayedValuePropertyOrdering2.js
@@ -0,0 +1,10 @@
+(function() {
+    let x = global.__abstract ? __abstract("boolean", "false") : false;
+    let o = { a: undefined, b: {} };
+    o.a = o; // delay
+    if (x) delete o.a; // might have been deleted
+
+    global.inspect = function() {
+      for (let name in o) { return name; }
+    };
+})();
