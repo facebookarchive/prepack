59e387bd82ad0a5903c39453070ea826120cee5f
YingHui Tan <jeffreytan@fb.com>
2017-07-31 10:40:31 -0700

    Add array initializer test

---
+++ test/serializer/abstract/ArrayInitializer.js
@@ -0,0 +1,11 @@
+(function() {
+  let x = global.__abstract ? __abstract("boolean", "true") : true;
+  let o = [];
+  o.push(11);
+  o.push(22);
+  if (x) {
+    o.push(3);
+  } else {
+  }
+  inspect = function() { return o; }
+})();
