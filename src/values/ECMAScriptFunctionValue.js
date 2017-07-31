5b1f55d5f14d5c88221f477238d3b6ff1c6e590b
Jeffrey Tan <jeffreytan@fb.com>
2017-07-31 10:42:30 -0700

    Refactor FunctionValue
    Summary:
    Introduce ECMAScriptFunctionValue and move many fields of FunctionValue down to ECMAScriptFunctionValue. FunctionValue becomes the abstract base class:
    1. Use invariant(F instanceof ECMAScriptFunctionValue) if only ECMAScriptFunctionValue is valid in that code path to make flow happy.
    2. Replace "F.getType() === FunctionValue" with Value.isTypeCompatibleWith(F.getType(), FunctionValue)
    3. $ScriptOrModule may also be able to move down to ECMAScriptFunctionValue, but I am not 100% positive so leave it in FunctionValue for now.
    Closes https://github.com/facebook/prepack/pull/824
    
    Differential Revision: D5527976
    
    Pulled By: yinghuitan
    
    fbshipit-source-id: 017748d67c31aa121c35b28b43cef026e904a412

---
+++ src/values/ECMAScriptFunctionValue.js
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+/* @flow */
+
+import type { Realm } from "../realm.js";
+import type { ObjectValue } from "./index.js";
+import { FunctionValue } from "./index.js";
+
+/* Abstract base class for non-exotic function objects(either with source or built-in) */
+export default class ECMAScriptFunctionValue extends FunctionValue {
+  constructor(realm: Realm, intrinsicName?: string) {
+    super(realm, intrinsicName);
+  }
+
+  $ConstructorKind: "base" | "derived";
+  $ThisMode: "lexical" | "strict" | "global";
+  $HomeObject: void | ObjectValue;
+  $FunctionKind: "normal" | "classConstructor" | "generator";
+}
