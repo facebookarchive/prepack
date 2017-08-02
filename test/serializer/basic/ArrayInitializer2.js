07bf0e3a26161c1dcd144f7c0229613fb00792f1
Jeffrey Tan <jeffreytan@fb.com>
2017-08-01 23:22:06 -0700

    Move array index properties to initializer list when its length is abstract
    Summary:
    When array length is abstract, prepack generates assignment for array index properties instead of using array initialization list. This PR fixed this issue. See #849 for new generated code for the test.
    Closes https://github.com/facebook/prepack/pull/854
    
    Differential Revision: D5541240
    
    Pulled By: yinghuitan
    
    fbshipit-source-id: 72b51bfd965feb1389aaff67156a357eda2a0696

---
+++ test/serializer/basic/ArrayInitializer2.js
@@ -0,0 +1,8 @@
+// does contain:[11,, 333, 44]
+(function() {
+  let o = [11, 22, 33, 44];
+  o["2"] = 333;     // Index property.
+  delete o[1];
+  o["010"] = 42;    // None-index property.
+  inspect = function() { return o; }
+})();
