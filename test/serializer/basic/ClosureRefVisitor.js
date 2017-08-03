21ba7be433338024b4ee2c50b22ce345ecb6a517
Herman Venter <hermanv@fb.com>
2017-08-03 11:47:51 -0700

    Catch fatal errors in visitName.

---
+++ test/serializer/basic/ClosureRefVisitor.js
@@ -0,0 +1,10 @@
+// jsc
+(function() {
+  let o = {
+    encode:function encode(s){
+      return unescape(encodeURI(s));
+    }
+  };
+  inspect = function() { return o; }
+  if (global.__makePartial) { __makePartial(global); }
+})();
