6ca6df1e3a11412b43931813eaefe6241a02c9b4
Nikolai Tillmann <NTillmann@users.noreply.github.com>
2017-08-25 12:45:33 -0700

    Slight improvement to factorifyObject
    Summary:
    Treat `void 0` (and similar patterns) as a simple value that factorifyObject can operate on.
    
    This slightly reduces code size with no adverse effects.
    Closes https://github.com/facebook/prepack/pull/909
    
    Differential Revision: D5675928
    
    Pulled By: NTillmann
    
    fbshipit-source-id: 399de8bc71e18ee4aa5aada8ca6e093bcb839fc5

---
+++ test/serializer/basic/FactorifyVoid0.js
@@ -0,0 +1,10 @@
+// Copies of void 0:1
+// do not inline expressions
+(function() {
+    a = { x: 1, y: undefined };
+    b = { x: 1, y: undefined };
+    c = { x: 1, y: undefined };
+    d = { x: 1, y: undefined };
+    e = { x: 1, y: undefined };
+    inspect = function() { a.x + b.x + c.x + d.x + e.x; }
+})();
