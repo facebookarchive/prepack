767abaf3878c2fe427d7ee3b6cd467f939465b8a
Herman Venter <hermanv@fb.com>
2017-09-20 16:29:33 -0700

    Simplify if conditions

--- src/utils/simplifier.js
+++ src/utils/simplifier.js
@@ -38,9 +38,54 @@ function simplify(realm, value: Value): Value {
   switch (value.kind) {
     case "!":
       return negate(realm, value.args[0]);
+    case "||":
+    case "&&":
+      let x = simplify(realm, value.args[0]);
+      let y = simplify(realm, value.args[1]);
+      if (x instanceof AbstractValue && x.equals(y)) return x;
+      return AbstractValue.createFromLogicalOp(realm, (value.kind: any), x, y, value.expressionLocation);
+    case "==":
+    case "!=":
+    case "===":
+    case "!==":
+      return simplifyEquality(realm, value);
     default:
       return value;
   }
+}
+
+function simplifyEquality(realm: Realm, equality: AbstractValue): Value {
+  let op = equality.kind;
+  let [x, y] = equality.args;
+  if (x instanceof ConcreteValue) [x, y] = [y, x];
+  if (x instanceof AbstractValue && x.kind === "conditional" && (!y.mightNotBeUndefined() || !y.mightNotBeNull())) {
+    // try to simplify "(cond ? xx : xy) op undefined/null" to just "cond" or "!cond"
+    let [cond, xx, xy] = x.args;
+    invariant(cond instanceof AbstractValue); // otherwise the the conditional should not have been created
+    if (op === "===" || op === "!==") {
+      // if xx === undefined && xy !== undefined then cond <=> x === undefined
+      if (!y.mightNotBeUndefined() && !xx.mightNotBeUndefined() && !xy.mightBeUndefined())
+        return op === "===" ? cond : negate(realm, cond);
+      // if xx !== undefined && xy === undefined then !cond <=> x === undefined
+      if (!y.mightNotBeUndefined() && !xx.mightBeUndefined() && !xy.mightNotBeUndefined())
+        return op === "===" ? negate(realm, cond) : cond;
+      // if xx === null && xy !== null then cond <=> x === null
+      if (!y.mightNotBeNull() && !xx.mightNotBeNull() && !xy.mightBeNull())
+        return op === "===" ? cond : negate(realm, cond);
+      // if xx !== null && xy === null then !cond <=> x === null
+      if (!y.mightNotBeNull() && !xx.mightBeNull() && !xy.mightNotBeNull())
+        return op === "===" ? negate(realm, cond) : cond;
+    } else {
+      invariant(op === "==" || op === "!=");
+      // if xx cannot be undefined/null and xy is undefined/null then !cond <=> x == undefined/null
+      if (!xx.mightBeUndefined() && !xx.mightBeNull() && (!xy.mightNotBeUndefined() || !xy.mightNotBeNull()))
+        return op === "==" ? negate(realm, cond) : cond;
+      // if xx is undefined/null and xy cannot be undefined/null then cond <=> x == undefined/null
+      if ((!xx.mightNotBeUndefined() || !xx.mightNotBeNull()) && !xy.mightBeUndefined() && !xy.mightBeNull())
+        return op === "==" ? cond : negate(realm, cond);
+    }
+  }
+  return equality;
 }
 
 function negate(realm: Realm, value: Value): Value {
---
+++ test/serializer/optimizations/simplify.js
@@ -0,0 +1,19 @@
+let b = global.__abstract ? __abstract("boolean", "true") : true;
+let ob1 = b ? { foo: 42 } : undefined;
+let ob2 = b ? { foo: 24 } : null;
+
+x = "no can do";
+if (ob1 == null || undefined == ob2)
+  x = "no can do";
+else {
+  x = ob1.foo;
+  y = ob2.foo;
+}
+if (ob1 != null) {
+  x = ob1.foo;
+}
+if (null == ob1) {
+  x = "no can do";
+}
+
+inspect = function() { return x + " " + y; }
