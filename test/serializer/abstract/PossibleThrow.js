80a93b6fa28ada588cda23f904cdea40809ab906
Joseph Zhou <josephzhou@fb.com>
2017-10-03 10:43:41 -0700

    Change error message when evaluating PossiblyNormalCompletion
    Summary:
    Issue: #976
     Use handleError with an message instead of reportIntrospectionError
    Closes https://github.com/facebook/prepack/pull/997
    
    Differential Revision: D5918135
    
    Pulled By: JWZ2018
    
    fbshipit-source-id: 41b432d17740741fa948e8546bc1ea2b99af9ed6

---
+++ test/serializer/abstract/PossibleThrow.js
@@ -0,0 +1,15 @@
+// throws introspection error
+let x = global.__abstract ? __abstract("boolean", "true") : true;
+let y;
+try {
+  if (x) {
+    y = 1;
+    throw Error();
+  } else {
+    y = 0;
+  }
+}
+catch(e) {
+}
+
+inspect = function(){return y}
