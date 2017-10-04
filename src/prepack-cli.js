324d8ae79447e7751f21454c52d7b4b3c0317e12
Pat Xu <patrickxu13@gmail.com>
2017-10-04 10:32:12 -0700

    add https:// prefix

--- src/prepack-cli.js
+++ src/prepack-cli.js
@@ -197,7 +197,7 @@ function run(
         console.log(
           `${loc.source || ""}(${loc.start.line}:${loc.start.column +
             1}) ${error.severity} ${error.errorCode}: ${error.message}` +
-            ` (github.com/facebook/prepack/wiki/${error.errorCode})`
+            ` (https://github.com/facebook/prepack/wiki/${error.errorCode})`
         );
       }
       if (foundFatal) process.exit(1);
