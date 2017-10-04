a90e866ed8eef6d22d37b2551ae3e81b69b8155a
Pat Xu <patrickxu13@gmail.com>
2017-10-04 08:58:17 -0700

    fix linter error

--- src/prepack-cli.js
+++ src/prepack-cli.js
@@ -196,8 +196,8 @@ function run(
         foundFatal = foundFatal || error.severity === "FatalError";
         console.log(
           `${loc.source || ""}(${loc.start.line}:${loc.start.column +
-            1}) ${error.severity} ${error.errorCode}: ${error.message}`
-            + ` (github.com/facebook/prepack/wiki/${error.errorCode})`
+            1}) ${error.severity} ${error.errorCode}: ${error.message}` +
+            ` (github.com/facebook/prepack/wiki/${error.errorCode})`
         );
       }
       if (foundFatal) process.exit(1);
