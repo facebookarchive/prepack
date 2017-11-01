d2689586b49154f30db293e2cd156b4cae6309b4
Wuhan Zhou <wuhan.joseph.zhou@gmail.com>
2017-11-01 12:08:55 -0700

    Consolidate debugger message parsing and formatting
    Summary:
    Release note: none
    
    - add a parser class to parse adapter commands
    - add a formatter class to format messages sent through the channels
    - move the message queue from the adapter into the AdapterChannel
    Closes https://github.com/facebook/prepack/pull/1114
    
    Differential Revision: D6211129
    
    Pulled By: JWZ2018
    
    fbshipit-source-id: f2466ed8edf644b7a834db840354646214c4f275

--- src/debugger/channel/AdapterChannel.js
+++ src/debugger/channel/AdapterChannel.js
@@ -10,20 +10,117 @@
 /* @flow */
 import type { DebuggerOptions } from "./../../options.js";
 import { FileIOWrapper } from "./FileIOWrapper.js";
+import { MessageMarshaller } from "./MessageMarshaller.js";
+import Queue from "queue-fifo";
+import EventEmitter from "events";
+import invariant from "./../../invariant.js";
+import { DebugMessage } from "./DebugMessage.js";
+import { DebuggerConstants } from "./../DebuggerConstants.js";
+import type { BreakpointArguments } from "./../types.js";
 
 //Channel used by the debug adapter to communicate with Prepack
 export class AdapterChannel {
   constructor(dbgOptions: DebuggerOptions) {
     this._ioWrapper = new FileIOWrapper(true, dbgOptions.inFilePath, dbgOptions.outFilePath);
+    this._marshaller = new MessageMarshaller();
+    this._queue = new Queue();
+    this._pendingRequestCallbacks = new Map();
+    this._eventEmitter = new EventEmitter();
+    this.sendDebuggerStart(DebuggerConstants.DEFAULT_REQUEST_ID);
+    this.listenOnFile(this._processPrepackMessage.bind(this));
   }
   _ioWrapper: FileIOWrapper;
+  _marshaller: MessageMarshaller;
+  _queue: Queue;
+  _pendingRequestCallbacks: { [number]: (string) => void };
+  _prepackWaiting: boolean;
+  _eventEmitter: EventEmitter;
+
+  // Error handler for errors in files from the adapter channel
+  _handleFileReadError(err: ?ErrnoError) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  _processPrepackMessage(message: string) {
+    let parts = message.split(" ");
+    let requestID = parseInt(parts[0], 10);
+    invariant(!isNaN(requestID));
+    let prefix = parts[1];
+    if (prefix === DebugMessage.PREPACK_READY_RESPONSE) {
+      this._prepackWaiting = true;
+      this._eventEmitter.emit(DebugMessage.PREPACK_READY_RESPONSE);
+      this.trySendNextRequest();
+    } else if (prefix === DebugMessage.BREAKPOINT_ADD_ACKNOWLEDGE) {
+      this._eventEmitter.emit(DebugMessage.BREAKPOINT_ADD_ACKNOWLEDGE, requestID, message);
+      // Prepack acknowledged adding a breakpoint
+      this._prepackWaiting = true;
+      this._processRequestCallback(requestID, message);
+      this.trySendNextRequest();
+    } else if (prefix === DebugMessage.BREAKPOINT_STOPPED_RESPONSE) {
+      this._eventEmitter.emit(DebugMessage.BREAKPOINT_STOPPED_RESPONSE, parts.slice(2).join(" "));
+      // Prepack stopped on a breakpoint
+      this._prepackWaiting = true;
+      this._processRequestCallback(requestID, message);
+      this.trySendNextRequest();
+    }
+  }
+
+  // Check to see if the next request to Prepack can be sent and send it if so
+  trySendNextRequest(): boolean {
+    // check to see if Prepack is ready to accept another request
+    if (!this._prepackWaiting) return false;
+    // check that there is a message to send
+    if (this._queue.isEmpty()) return false;
+    let request = this._queue.dequeue();
+    this.listenOnFile(this._processPrepackMessage.bind(this));
+    this.writeOut(request);
+    this._prepackWaiting = false;
+    return true;
+  }
+
+  _addRequestCallback(requestID: number, callback: string => void) {
+    invariant(!(requestID in this._pendingRequestCallbacks), "Request ID already exists in pending requests");
+    this._pendingRequestCallbacks[requestID] = callback;
+  }
+
+  _processRequestCallback(requestID: number, message: string) {
+    invariant(
+      requestID in this._pendingRequestCallbacks,
+      "Request ID does not exist in pending requests: " + requestID
+    );
+    let callback = this._pendingRequestCallbacks[requestID];
+    callback(message);
+  }
+
+  registerChannelEvent(event: string, listener: (message: string) => void) {
+    this._eventEmitter.addListener(event, listener);
+  }
+
+  run(requestID: number, callback: string => void) {
+    this._queue.enqueue(this._marshaller.marshallContinueRequest(requestID));
+    this.trySendNextRequest();
+    this._addRequestCallback(requestID, callback);
+  }
+
+  setBreakpoints(requestID: number, breakpoints: Array<BreakpointArguments>, callback: string => void) {
+    for (const breakpoint of breakpoints) {
+      this._queue.enqueue(this._marshaller.marshallSetBreakpointsRequest(breakpoint));
+    }
+    this.trySendNextRequest();
+    this._addRequestCallback(requestID, callback);
+  }
 
   writeOut(contents: string) {
     this._ioWrapper.writeOutSync(contents);
   }
 
-  listenOnFile(errorHandler: (err: ?ErrnoError) => void, messageProcessor: (message: string) => void) {
-    this._ioWrapper.readIn(errorHandler, messageProcessor);
+  sendDebuggerStart(requestID: number) {
+    this.writeOut(this._marshaller.marshallDebuggerStart(requestID));
+  }
+
+  listenOnFile(messageProcessor: (message: string) => void) {
+    this._ioWrapper.readIn(this._handleFileReadError.bind(this), messageProcessor);
   }
 
   clean() {
