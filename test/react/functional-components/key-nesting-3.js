7e9461c100581817fcdd446b31f2de830fdb3e05
Dominic Gannaway <dg@domgan.com>
2017-11-14 13:57:15 +0000

    Improved key handling and removed un-needed code + more tests

---
+++ test/react/functional-components/key-nesting-3.js
@@ -0,0 +1,33 @@
+var React = require('react');
+
+// the JSX transform converts to React, so we need to add it back in
+this['React'] = React;
+
+function Child() {
+	var x = [];
+
+	for (let i = 0; i < 10; i++) {
+		x.push(<span key={i} />)
+	}
+  return x;
+}
+
+function App() {
+  return <div>
+    <span />
+    <Child />
+    <Child />
+		<span />
+  </div>
+}
+
+App.getTrials = function(renderer, Root) {
+  renderer.update(<Root />);
+  return [['key nesting 3', renderer.toJSON()]];
+};
+
+if (this.__registerReactComponentRoot) {
+  __registerReactComponentRoot(App);
+}
+
+module.exports = App;
