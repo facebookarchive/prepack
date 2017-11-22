3cd5ca1a8928f6dc925204eed084b9f96f221b8f
Wuhan Zhou <wuhan.joseph.zhou@gmail.com>
2017-11-22 10:50:37 -0800

    Fix lint on master
    Summary:
    Release note: none
    Closes https://github.com/facebook/prepack/pull/1185
    
    Differential Revision: D6397761
    
    Pulled By: JWZ2018
    
    fbshipit-source-id: 85e4c3fcf7f837ed3dad9729e5f18e5bf0c17ca3

--- scripts/test-internal.js
+++ scripts/test-internal.js
@@ -86,10 +86,7 @@ function runTest(name: string, code: string): boolean {
       sourceMaps: !!sourceMap,
       additionalFunctions: additionalFunctions,
     };
-    if (name.endsWith("/bundle.js~"))
-      (options: any).additionalFunctions = [
-        "global.WildeBundle.prepareReact",
-      ];
+    if (name.endsWith("/bundle.js~")) (options: any).additionalFunctions = ["global.WildeBundle.prepareReact"];
     let serialized = prepackSources(sources, options);
     let new_map = serialized.map; // force source maps to get computed
     if (!new_map) console.log(chalk.red("No source map"));
