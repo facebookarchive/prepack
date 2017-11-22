27d1920137330afe12843525f47a992d2aa530dc
Herman Venter <hermanv@fb.com>
2017-11-21 17:27:54 -0800

    Avoid unused property reads on simple objects

---
+++ test/serializer/basic/PropertyAssignment.js
@@ -0,0 +1,6 @@
+// add at runtime:var foo = {};
+// Copies of .bar:2
+var ob = global.__makeSimple ? __makeSimple(__abstract({}, "foo")) :  {};
+ob.bar = {a: 1};
+
+inspect = function() { return JSON.stringify(ob.bar); }
