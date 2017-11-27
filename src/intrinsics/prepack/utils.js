0c94a731e2c223a650ec115668c51f82425debbf
Dominic Gannaway <dg@domgan.com>
2017-11-27 02:42:08 -0800

    Adds more document mock abstracts
    Summary:
    This refactors https://github.com/facebook/prepack/pull/1190 slightly to use it in a more general way. This PR also adds some more `document` object mock abstracts.
    Closes https://github.com/facebook/prepack/pull/1192
    
    Differential Revision: D6413273
    
    Pulled By: trueadm
    
    fbshipit-source-id: 6467377c700c6ac0900cfee72762da20af635f89

--- src/intrinsics/prepack/utils.js
+++ src/intrinsics/prepack/utils.js
@@ -15,7 +15,6 @@ import {
   AbstractValue,
   ConcreteValue,
   FunctionValue,
-  NativeFunctionValue,
   StringValue,
   ObjectValue,
   UndefinedValue,
@@ -24,16 +23,23 @@ import buildExpressionTemplate from "../../utils/builder.js";
 import { describeLocation } from "../ecma262/Error.js";
 import { ToStringPartial } from "../../methods/index.js";
 import { ValuesDomain } from "../../domains/index.js";
+import AbstractObjectValue from "../../values/AbstractObjectValue";
 
 const throwTemplateSrc = "(function(){throw new global.Error('abstract value defined at ' + A);})()";
 const throwTemplate = buildExpressionTemplate(throwTemplateSrc);
 
 export function parseTypeNameOrTemplate(
   realm: Realm,
-  typeNameOrTemplate: void | Value
+  typeNameOrTemplate: void | Value | string
 ): { type: typeof Value, template: void | ObjectValue } {
   if (typeNameOrTemplate === undefined || typeNameOrTemplate instanceof UndefinedValue) {
     return { type: Value, template: undefined };
+  } else if (typeof typeNameOrTemplate === "string") {
+    let type = Value.getTypeFromName(typeNameOrTemplate);
+    if (type === undefined) {
+      throw realm.createErrorThrowCompletion(realm.intrinsics.TypeError, "unknown typeNameOrTemplate");
+    }
+    return { type, template: undefined };
   } else if (typeNameOrTemplate instanceof StringValue) {
     let typeNameString = ToStringPartial(realm, typeNameOrTemplate);
     let type = Value.getTypeFromName(typeNameString);
@@ -50,52 +56,49 @@ export function parseTypeNameOrTemplate(
   }
 }
 
-export function createAbstract(realm: Realm, ...additionalValues: Array<ConcreteValue>): NativeFunctionValue {
-  return new NativeFunctionValue(
-    realm,
-    "global.__abstract",
-    "__abstract",
-    0,
-    (context, [typeNameOrTemplate: void | Value, name]) => {
-      if (!realm.useAbstractInterpretation) {
-        throw realm.createErrorThrowCompletion(realm.intrinsics.TypeError, "realm is not partial");
-      }
+export function createAbstract(
+  realm: Realm,
+  typeNameOrTemplate?: Value | string,
+  name?: string,
+  ...additionalValues: Array<ConcreteValue>
+): AbstractValue | AbstractObjectValue {
+  if (!realm.useAbstractInterpretation) {
+    throw realm.createErrorThrowCompletion(realm.intrinsics.TypeError, "realm is not partial");
+  }
 
-      let { type, template } = parseTypeNameOrTemplate(realm, typeNameOrTemplate);
+  let { type, template } = parseTypeNameOrTemplate(realm, typeNameOrTemplate);
 
-      let result;
-      let nameString = name ? ToStringPartial(realm, name) : "";
-      if (nameString === "") {
-        let locString;
-        for (let executionContext of realm.contextStack.slice().reverse()) {
-          let caller = executionContext.caller;
-          locString = describeLocation(
-            realm,
-            caller ? caller.function : undefined,
-            caller ? caller.lexicalEnvironment : undefined,
-            executionContext.loc
-          );
-          if (locString !== undefined) break;
-        }
-        let locVal = new StringValue(realm, locString || "(unknown location)");
-        let kind = "__abstract_" + realm.objectCount++; // need not be an object, but must be unique
-        result = AbstractValue.createFromTemplate(realm, throwTemplate, type, [locVal], kind);
-      } else {
-        let kind = "__abstract_" + nameString; // assume name is unique TODO #1155: check this
-        result = AbstractValue.createFromTemplate(realm, buildExpressionTemplate(nameString), type, [], kind);
-        result.intrinsicName = nameString;
-      }
-
-      if (template) result.values = new ValuesDomain(new Set([template]));
-      if (template && !(template instanceof FunctionValue)) {
-        // why exclude functions?
-        template.makePartial();
-        if (nameString) realm.rebuildNestedProperties(result, nameString);
-      }
-
-      if (additionalValues.length > 0)
-        result = AbstractValue.createAbstractConcreteUnion(realm, result, ...additionalValues);
-      return result;
+  let result;
+  let nameString = name ? ToStringPartial(realm, name) : "";
+  if (nameString === "") {
+    let locString;
+    for (let executionContext of realm.contextStack.slice().reverse()) {
+      let caller = executionContext.caller;
+      locString = describeLocation(
+        realm,
+        caller ? caller.function : undefined,
+        caller ? caller.lexicalEnvironment : undefined,
+        executionContext.loc
+      );
+      if (locString !== undefined) break;
     }
-  );
+    let locVal = new StringValue(realm, locString || "(unknown location)");
+    let kind = "__abstract_" + realm.objectCount++; // need not be an object, but must be unique
+    result = AbstractValue.createFromTemplate(realm, throwTemplate, type, [locVal], kind);
+  } else {
+    let kind = "__abstract_" + nameString; // assume name is unique TODO #1155: check this
+    result = AbstractValue.createFromTemplate(realm, buildExpressionTemplate(nameString), type, [], kind);
+    result.intrinsicName = nameString;
+  }
+
+  if (template) result.values = new ValuesDomain(new Set([template]));
+  if (template && !(template instanceof FunctionValue)) {
+    // why exclude functions?
+    template.makePartial();
+    if (nameString) realm.rebuildNestedProperties(result, nameString);
+  }
+
+  if (additionalValues.length > 0)
+    result = AbstractValue.createAbstractConcreteUnion(realm, result, ...additionalValues);
+  return result;
 }
