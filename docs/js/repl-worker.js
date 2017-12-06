c22608aa2905940fa7d0c2b2176ddf3361e6f5c1
James Nolan <jamesnolan@fb.com>
2017-12-06 15:42:03 -0800

    Copy content of gh-pages branch in docs/ directory of master branch

---
+++ docs/js/repl-worker.js
@@ -0,0 +1,48 @@
+self.importScripts('prepack.min.js');
+
+onmessage = function(e) {
+  var buffer = [];
+
+  function errorHandler(error) {
+    // Syntax errors contain their location at the end, remove that
+    if (error.errorCode === 'PP1004') {
+      let msg = error.message;
+      error.message = msg.substring(0, msg.lastIndexOf('('));
+    }
+    buffer.push({
+      severity: error.severity,
+      location: error.location,
+      message: error.message,
+      errorCode: error.errorCode,
+    });
+    return 'Recover';
+  }
+
+  try {
+    var sources = [{ filePath: 'dummy', fileContents: e.data.code }];
+    var options = {
+      compatibility: 'browser',
+      filename: 'repl',
+      timeout: 1000,
+      serialize: true,
+      errorHandler,
+    };
+    for (var property in e.data.options) {
+      if (e.data.options.hasOwnProperty(property)) {
+        options[property] = e.data.options[property];
+      }
+    }
+    var result = Prepack.prepackSources(sources, options);
+    if (result && !buffer.length) {
+      postMessage({ type: 'success', data: result.code });
+    } else {
+      // A well-defined error occurred.
+      postMessage({ type: 'error', data: buffer });
+    }
+  } catch (err) {
+    buffer.push({
+      message: err.message || 'An unknown error occurred'
+    });
+    postMessage({ type: 'error', data: buffer });
+  }
+};
