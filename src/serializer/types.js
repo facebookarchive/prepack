e50d03292ed594aa5dcf31e69f766ab8ddd994e3
YingHui Tan <jeffreytan@fb.com>
2017-12-07 15:45:28 -0800

    Remove getSerializeObjectIdentifierOptional

--- src/serializer/types.js
+++ src/serializer/types.js
@@ -180,7 +180,7 @@ export class SerializerStatistics {
 }
 
 export type LocationService = {
-  getLocation: Value => void | BabelNodeIdentifier,
+  getLocation: Value => BabelNodeIdentifier,
   createLocation: () => BabelNodeIdentifier,
 };
 
