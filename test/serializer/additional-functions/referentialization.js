99919887fe042d34fba40a0fd508d64a0eb92f99
YingHui Tan <jeffreytan@fb.com>
2017-12-21 09:58:34 -0800

    Fix additional function mutable binding function scope

---
+++ test/serializer/additional-functions/referentialization.js
@@ -0,0 +1,18 @@
+(function() {
+  function additional() {
+    var obj = {};
+    global.nested = function() {
+      obj = {};
+      obj.p = 100;
+      return obj;
+    };
+    return obj;
+  }
+  if (global.__registerAdditionalFunctionToPrepack) {
+    global.__registerAdditionalFunctionToPrepack(additional);
+  }
+  inspect = function() {
+    additional();
+    return JSON.stringify(nested());
+  };
+})();
