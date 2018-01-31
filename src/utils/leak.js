bfacb092d83ee2dff9ae28d0c42719ef74950054
Dan Abramov <dan.abramov@gmail.com>
2018-01-31 18:20:56 +0000

    Revert unnecessary change

--- src/utils/leak.js
+++ src/utils/leak.js
@@ -362,10 +362,7 @@ class ObjectValueLeakingVisitor {
         this.visitValueSet(val);
         return;
       default:
-        invariant(
-          kind === "Object" || kind === "Array",
-          `Object of kind ${kind} is not supported in calls to abstract functions.`
-        );
+        invariant(kind === "Object", `Object of kind ${kind} is not supported in calls to abstract functions.`);
         invariant(
           this.$ParameterMap === undefined,
           `Arguments object is not supported in calls to abstract functions.`
