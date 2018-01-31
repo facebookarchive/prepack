78320e24c9b623869593c890bdaca31e4fbebb4c
Dan Abramov <dan.abramov@gmail.com>
2018-01-31 17:35:37 +0000

    Fix the test

--- test/react/mocks/fb4.js
+++ test/react/mocks/fb4.js
@@ -5,4 +5,4 @@ App.arr = [1, 2, 3];
 App.otherArray = [];
 App.otherArray.length = 10;
 
-global.WrappedApp = require("RelayModern").createFragmentContainer(App);
+this.WrappedApp = require("RelayModern").createFragmentContainer(App);
