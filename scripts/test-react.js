20699a01d27ac9219f3c41c38f9a528c4073db1e
Dominic Gannaway <dg@domgan.com>
2018-02-13 22:04:28 +0000

    added React test

--- scripts/test-react.js
+++ scripts/test-react.js
@@ -185,6 +185,10 @@ function runTestSuite(outputJsx) {
         await runTest(directory, "simple-6.js");
       });
 
+      it("Simple with Object.assign", async () => {
+        await runTest(directory, "simple-with-object-assign.js");
+      });
+
       it("Simple fragments", async () => {
         await runTest(directory, "simple-fragments.js");
       });
