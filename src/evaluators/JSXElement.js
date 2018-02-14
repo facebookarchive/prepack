d0e49e591228292a231f81b1da2b21681e4e4b9e
Dominic Gannaway <dg@domgan.com>
2018-02-14 14:28:29 +0000

    React.createElement now uses the same path as JSX

--- src/evaluators/JSXElement.js
+++ src/evaluators/JSXElement.js
@@ -32,14 +32,14 @@ import {
   ObjectValue,
   FunctionValue,
   AbstractValue,
+  SymbolValue,
 } from "../values/index.js";
-import { getReactSymbol } from "../react/utils.js";
 import { convertJSXExpressionToIdentifier } from "../react/jsx.js";
 import * as t from "babel-types";
 import { Get } from "../methods/index.js";
 import { Create, Environment, Properties } from "../singletons.js";
 import invariant from "../invariant.js";
-import { computeBinary } from "./BinaryExpression.js";
+import { createReactElement } from "../react/elements.js";
 
 // taken from Babel
 function cleanJSXElementLiteralChild(child: string): null | string {
@@ -145,37 +145,14 @@ function isTagName(ast: BabelNode): boolean {
   return ast.type === "JSXIdentifier" && /^[a-z]|\-/.test(((ast: any): BabelNodeJSXIdentifier).name);
 }
 
-function getDefaultProps(
-  elementType: BabelNodeJSXIdentifier | BabelNodeJSXMemberExpression,
-  env,
-  realm: Realm
-): null | ObjectValue {
-  let name;
-  if (elementType.type === "JSXIdentifier") {
-    name = elementType.name;
-  }
-  if (!isTagName(elementType) && typeof name === "string") {
-    // find the value of "ComponentXXX.defaultProps"
-    let defaultProps = Environment.GetValue(
-      realm,
-      env.evaluate(t.memberExpression(t.identifier(name), t.identifier("defaultProps")), false)
-    );
-
-    if (defaultProps instanceof ObjectValue) {
-      return defaultProps;
-    }
-  }
-  return null;
-}
-
 function evaluateJSXChildren(
   children: Array<BabelNode>,
   strictCode: boolean,
   env: LexicalEnvironment,
   realm: Realm
-): ArrayValue | Value | null {
+): ArrayValue | Value {
   if (children.length === 0) {
-    return null;
+    return realm.intrinsics.undefined;
   }
   if (children.length === 1) {
     let singleChild = evaluateJSXValue(children[0], strictCode, env, realm);
@@ -191,7 +168,7 @@ function evaluateJSXChildren(
   let array = Create.ArrayCreate(realm, 0);
   let dynamicChildrenLength = children.length;
   let dynamicIterator = 0;
-  let lastChildValue = null;
+  let lastChildValue = realm.intrinsics.undefined;
   for (let i = 0; i < children.length; i++) {
     let value = evaluateJSXValue(children[i], strictCode, env, realm);
     if (value instanceof StringValue) {
@@ -217,68 +194,23 @@ function evaluateJSXChildren(
 }
 
 function evaluateJSXAttributes(
-  elementType: BabelNodeJSXIdentifier | BabelNodeJSXMemberExpression,
   astAttributes: Array<BabelNodeJSXAttribute | BabelNodeJSXSpreadAttribute>,
-  astChildren: Array<BabelNode>,
   strictCode: boolean,
   env: LexicalEnvironment,
   realm: Realm
-): { key: Value, props: ObjectValue | AbstractObjectValue, ref: Value } {
-  let props = Create.ObjectCreate(realm, realm.intrinsics.ObjectPrototype);
-  let children = evaluateJSXChildren(astChildren, strictCode, env, realm);
-  let key = realm.intrinsics.null;
-  let ref = realm.intrinsics.null;
-  let defaultProps = getDefaultProps(elementType, env, realm);
-  // to be used if we need to create an abstract props
+): ObjectValue | AbstractObjectValue {
+  let config = Create.ObjectCreate(realm, realm.intrinsics.ObjectPrototype);
   let abstractPropsArgs = [];
-
   let containsAbstractSpreadAttribute = false;
-  let containsAbstractKey = false;
-  let containsAbstractRef = false;
   let spreadAttributesCount = 0;
   let spreadAttributesWithInitialPropsHintCount = 0;
   let attributesAssigned = 0;
 
-  const setProp = (name: string, value: Value, isDefaultProps: boolean): void => {
-    if (name === "children") {
-      if (isDefaultProps && children !== null) {
-        return;
-      }
-      invariant(props instanceof ObjectValue);
-      Properties.Set(realm, props, "children", value, true);
-    } else if (name === "key" && value !== realm.intrinsics.null) {
-      if (value instanceof AbstractValue) {
-        containsAbstractKey = true;
-      } else if (containsAbstractKey) {
-        containsAbstractKey = false;
-      }
-      key = computeBinary(realm, "+", realm.intrinsics.emptyString, value);
-    } else if (name === "ref") {
-      if (value instanceof AbstractValue) {
-        containsAbstractRef = true;
-      } else if (containsAbstractRef) {
-        containsAbstractRef = false;
-      }
-      ref = value;
-    } else if (name !== "__self" && name !== "__source") {
-      invariant(props instanceof ObjectValue);
-      Properties.Set(realm, props, name, value, true);
-    }
+  const setConfigProperty = (name: string, value: Value): void => {
+    invariant(config instanceof ObjectValue);
+    Properties.Set(realm, config, name, value, true);
     attributesAssigned++;
   };
-
-  // handle children
-  if (children !== null) {
-    setProp("children", children, false);
-  }
-
-  // defaultProps are a bit like default function arguments
-  // if an actual value exists, it should overwrite the default value
-  if (defaultProps !== null) {
-    for (let [defaultPropKey] of defaultProps.properties) {
-      setProp(defaultPropKey, Get(realm, defaultProps, defaultPropKey), true);
-    }
-  }
 
   for (let astAttribute of astAttributes) {
     switch (astAttribute.type) {
@@ -286,14 +218,14 @@ function evaluateJSXAttributes(
         let { name, value } = astAttribute;
 
         invariant(name.type === "JSXIdentifier", `JSX attribute name type not supported: ${astAttribute.type}`);
-        setProp(name.name, evaluateJSXValue(((value: any): BabelNodeJSXIdentifier), strictCode, env, realm), false);
+        setConfigProperty(name.name, evaluateJSXValue(((value: any): BabelNodeJSXIdentifier), strictCode, env, realm));
         break;
       case "JSXSpreadAttribute":
         let spreadValue = Environment.GetValue(realm, env.evaluate(astAttribute.argument, strictCode));
 
         if (spreadValue instanceof ObjectValue) {
           for (let [spreadPropKey] of spreadValue.properties) {
-            setProp(spreadPropKey, Get(realm, spreadValue, spreadPropKey), false);
+            setConfigProperty(spreadPropKey, Get(realm, spreadValue, spreadPropKey));
           }
         } else {
           spreadAttributesCount++;
@@ -304,29 +236,23 @@ function evaluateJSXAttributes(
 
           if (reactHint === "HAS_NO_KEY_OR_REF") {
             spreadAttributesWithInitialPropsHintCount++;
-            // as we know initial props can't have a "props" or "key", we don't have
-            // to mark them as abstract
-          } else {
-            // as the spread might contain "key" and "ref" we have to mark both
-            // as being abstract too, as we don't know if they exist
-            containsAbstractKey = true;
-            containsAbstractRef = true;
           }
           // we push the props up to this point into the abstract props args. we also
           // push the abstract spread object and then we create a fresh props object
-          abstractPropsArgs.push(props, spreadValue);
-          props = Create.ObjectCreate(realm, realm.intrinsics.ObjectPrototype);
+          abstractPropsArgs.push(config, spreadValue);
+          config = Create.ObjectCreate(realm, realm.intrinsics.ObjectPrototype);
         }
         break;
       default:
         invariant(false, `Unknown JSX attribute type: ${astAttribute.type}`);
     }
   }
+
   if (containsAbstractSpreadAttribute) {
     // if we haven't assigned any attributes and we are dealing with a single
     // spread attribute, we can just make the spread object the props
     if (attributesAssigned === 0) {
-      props = abstractPropsArgs[1];
+      config = abstractPropsArgs[1];
     } else {
       // we create an abstract Object.assign() to deal with the fact that we don't what
       // the props are because they contain abstract spread attributes that we can't
@@ -335,7 +261,7 @@ function evaluateJSXAttributes(
       let values = new ValuesDomain();
       let emptyObject = Create.ObjectCreate(realm, realm.intrinsics.ObjectPrototype);
       invariant(realm.generator);
-      props = realm.generator.derive(types, values, [emptyObject, ...abstractPropsArgs], _args => {
+      config = realm.generator.derive(types, values, [emptyObject, ...abstractPropsArgs], _args => {
         return t.callExpression(
           t.memberExpression(t.identifier("Object"), t.identifier("assign")),
           ((_args: any): Array<any>)
@@ -345,39 +271,12 @@ function evaluateJSXAttributes(
         spreadAttributesCount === spreadAttributesWithInitialPropsHintCount &&
         spreadAttributesWithInitialPropsHintCount > 0
       ) {
-        realm.react.abstractHints.set(props, "HAS_NO_KEY_OR_REF");
-      }
-      if (containsAbstractKey || containsAbstractRef) {
-        // if either are abstract, this will impact the reconcilation process
-        // and ultimately prevent us from folding ReactElements properly
-        // so we unsafely allow this for now, but show a warning
-        invariant(realm.react.logger);
-        realm.react.logger.logWarning(
-          props,
-          `unable to evaluate "key" and "ref" on a ReactElement due to a JSXSpreadAttribute`
-        );
+        realm.react.abstractHints.set(config, "HAS_NO_KEY_OR_REF");
       }
     }
   }
-  invariant(props instanceof ObjectValue || props instanceof AbstractObjectValue);
-  return { key, props, ref };
-}
-
-function createReactElement(
-  realm: Realm,
-  type: Value,
-  key: Value,
-  ref: Value,
-  props: ObjectValue | AbstractObjectValue
-): ObjectValue {
-  let obj = Create.ObjectCreate(realm, realm.intrinsics.ObjectPrototype);
-  Create.CreateDataPropertyOrThrow(realm, obj, "$$typeof", getReactSymbol("react.element", realm));
-  Create.CreateDataPropertyOrThrow(realm, obj, "type", type);
-  Create.CreateDataPropertyOrThrow(realm, obj, "key", key);
-  Create.CreateDataPropertyOrThrow(realm, obj, "ref", ref);
-  Create.CreateDataPropertyOrThrow(realm, obj, "props", props);
-  Create.CreateDataPropertyOrThrow(realm, obj, "_owner", realm.intrinsics.null);
-  return obj;
+  invariant(config instanceof ObjectValue || config instanceof AbstractObjectValue);
+  return config;
 }
 
 export default function(
@@ -389,14 +288,13 @@ export default function(
   invariant(realm.react.enabled, "JSXElements can only be evaluated with the reactEnabled option");
   let openingElement = ast.openingElement;
   let type = evaluateJSXIdentifier(openingElement.name, strictCode, env, realm);
-  let { key, props, ref } = evaluateJSXAttributes(
-    openingElement.name,
-    openingElement.attributes,
-    ast.children,
-    strictCode,
-    env,
-    realm
+  let children = evaluateJSXChildren(ast.children, strictCode, env, realm);
+  let config = evaluateJSXAttributes(openingElement.attributes, strictCode, env, realm);
+  invariant(
+    type instanceof ObjectValue ||
+      type instanceof AbstractObjectValue ||
+      type instanceof StringValue ||
+      type instanceof SymbolValue
   );
-
-  return createReactElement(realm, type, key, ref, props);
+  return createReactElement(realm, type, config, children);
 }
---
+++ src/react/elements.js
@@ -0,0 +1,132 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+/* @flow */
+
+import type { Realm } from "../realm.js";
+import {
+  AbstractObjectValue,
+  StringValue,
+  Value,
+  ObjectValue,
+  FunctionValue,
+  NullValue,
+  SymbolValue,
+} from "../values/index.js";
+import { Create, Properties } from "../singletons.js";
+import { TypesDomain, ValuesDomain } from "../domains/index.js";
+import invariant from "../invariant.js";
+import { Get } from "../methods/index.js";
+import { getReactSymbol } from "./utils.js";
+import * as t from "babel-types";
+import { computeBinary } from "../evaluators/BinaryExpression.js";
+
+function createPropsObject(
+  realm: Realm,
+  type: ObjectValue | AbstractObjectValue | StringValue | SymbolValue,
+  config: ObjectValue | AbstractObjectValue | NullValue,
+  children: Value
+) {
+  let defaultProps = type instanceof ObjectValue ? Get(realm, type, "defaultProps") : null;
+  let props = Create.ObjectCreate(realm, realm.intrinsics.ObjectPrototype);
+  let key = realm.intrinsics.null;
+  let ref = realm.intrinsics.null;
+
+  const setProp = (name: string, value: Value): void => {
+    if (name === "children") {
+      invariant(props instanceof ObjectValue);
+      Properties.Set(realm, props, "children", value, true);
+    } else if (name === "key" && value !== realm.intrinsics.null) {
+      key = computeBinary(realm, "+", realm.intrinsics.emptyString, value);
+    } else if (name === "ref") {
+      ref = value;
+    } else if (name !== "__self" && name !== "__source") {
+      invariant(props instanceof ObjectValue);
+      Properties.Set(realm, props, name, value, true);
+    }
+  };
+
+  if (config instanceof AbstractObjectValue && config.isPartialObject()) {
+    // if we have defaultProps or children, we need to create a new merge of the objects
+    // along with our config
+    if (defaultProps !== realm.intrinsics.undefined || children !== realm.intrinsics.undefined) {
+      let args = [];
+      if (defaultProps !== realm.intrinsics.undefined) {
+        args.push(defaultProps);
+      }
+      args.push(config);
+      if (children !== realm.intrinsics.undefined) {
+        args.push(children);
+      }
+      let emptyObject = Create.ObjectCreate(realm, realm.intrinsics.ObjectPrototype);
+      let types = new TypesDomain(FunctionValue);
+      let values = new ValuesDomain();
+      invariant(realm.generator);
+      props = realm.generator.derive(types, values, [emptyObject, ...args], _args => {
+        return t.callExpression(
+          t.memberExpression(t.identifier("Object"), t.identifier("assign")),
+          ((_args: any): Array<any>)
+        );
+      });
+      invariant(config instanceof AbstractObjectValue);
+    } else {
+      props = config;
+    }
+    let reactHint = realm.react.abstractHints.get(config);
+
+    if (reactHint !== "HAS_NO_KEY_OR_REF") {
+      // if either are abstract, this will impact the reconcilation process
+      // and ultimately prevent us from folding ReactElements properly
+      // so we unsafely allow this for now, but show a warning
+      invariant(realm.react.logger);
+      realm.react.logger.logWarning(
+        config,
+        `unable to evaluate "key" and "ref" on a ReactElement due to a JSXSpreadAttribute`
+      );
+    }
+  } else {
+    if (config instanceof ObjectValue) {
+      for (let [propKey] of config.properties) {
+        setProp(propKey, Get(realm, config, propKey));
+      }
+    }
+
+    if (children !== realm.intrinsics.undefined) {
+      setProp("children", children);
+    }
+
+    if (defaultProps instanceof ObjectValue) {
+      for (let [propKey] of defaultProps.properties) {
+        if (Get(realm, props, propKey) === realm.intrinsics.undefined) {
+          setProp(propKey, Get(realm, defaultProps, propKey));
+        }
+      }
+    }
+  }
+
+  return { key, props, ref };
+}
+
+export function createReactElement(
+  realm: Realm,
+  type: ObjectValue | AbstractObjectValue | StringValue | SymbolValue,
+  config: ObjectValue | AbstractObjectValue | NullValue,
+  children: Value
+) {
+  let { key, props, ref } = createPropsObject(realm, type, config, children);
+
+  let obj = Create.ObjectCreate(realm, realm.intrinsics.ObjectPrototype);
+  Create.CreateDataPropertyOrThrow(realm, obj, "$$typeof", getReactSymbol("react.element", realm));
+  Create.CreateDataPropertyOrThrow(realm, obj, "type", type);
+  Create.CreateDataPropertyOrThrow(realm, obj, "key", key);
+  Create.CreateDataPropertyOrThrow(realm, obj, "ref", ref);
+  Create.CreateDataPropertyOrThrow(realm, obj, "props", props);
+  Create.CreateDataPropertyOrThrow(realm, obj, "_owner", realm.intrinsics.null);
+  return obj;
+}
