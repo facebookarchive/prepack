0f44edaace5e05740bbb084728a2aba380b41b38
Dan Abramov <dan.abramov@gmail.com>
2018-02-15 22:15:52 +0000

    Add a failing Object.assign serialization test

--- test/serializer/abstract/ObjectAssign5.js
+++ test/serializer/abstract/ObjectAssign5.js
@@ -1,4 +1,3 @@
-var obj = global.__abstract && global.__makePartial && global.__makeSimple ? __makeSimple(__makePartial(__abstract({}, "({foo:1})"))) : {foo:1};
 var copyOfObj = Object.assign({}, obj);
 var copyOfCopyOfObj = Object.assign({}, copyOfObj);
 
