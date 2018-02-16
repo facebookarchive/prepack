df43524fdbf60511e3d6e2310952370c61aeb097
Dominic Gannaway <dg@domgan.com>
2018-02-16 05:14:45 -0800

    Properly handle React functional component argument names
    Summary:
    Release notes: none
    
    Fixes https://github.com/facebook/prepack/pull/1430
    Closes https://github.com/facebook/prepack/pull/1453
    
    Reviewed By: gaearon
    
    Differential Revision: D6999763
    
    Pulled By: trueadm
    
    fbshipit-source-id: 15c97da4e87b140ad83ab945f33140ec9e51838d

--- scripts/test-react.js
+++ scripts/test-react.js
@@ -186,6 +186,10 @@ function runTestSuite(outputJsx) {
         await runTest(directory, "simple-6.js");
       });
 
+      it("Simple 7", async () => {
+        await runTest(directory, "simple-7.js");
+      });
+
       it("Simple fragments", async () => {
         await runTest(directory, "simple-fragments.js");
       });
