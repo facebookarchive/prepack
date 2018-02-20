856ce142b1789f8b45ede6238ea50e42ee5aa8fb
Dominic Gannaway <dg@domgan.com>
2018-02-20 22:59:45 +0000

    Support binary + and add more tests
    fix
    
    fix

---
+++ test/serializer/pure-functions/BinaryExpressions.js
@@ -0,0 +1,28 @@
+// additional functions
+// abstract effects
+
+let obj1 = global.__abstract ? __abstract({}, '({foo: {valueOf() { return 42; }}})') : {foo: {valueOf() { return 42; }}};
+let obj2 = global.__abstract ? __abstract({}, '({foo: {bar: {valueOf() { return 42; }}}})') : {foo: {bar: {valueOf() { return 42; }}}};
+
+if (global.__makePartial) {
+  __makePartial(obj1);
+  __makePartial(obj2);
+}
+if (global.__makeSimple) {
+  __makeSimple(obj1);
+  __makeSimple(obj2);
+}
+
+function additional1() {
+  return '' + obj1.foo;
+}
+
+function additional2() {
+  return '' + obj2.foo.bar;
+}
+
+inspect = function() {
+  let ret1 = additional1();
+  let ret2 = additional2();
+  return ret1 + ret2;
+}
