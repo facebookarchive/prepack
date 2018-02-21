a9d7729d807531c27028a346ae0b097eeb3a6bd4
Dan Abramov <dan.abramov@gmail.com>
2018-02-21 00:00:48 +0000

    Update BinaryExpressions2.js

--- test/serializer/pure-functions/BinaryExpressions2.js
+++ test/serializer/pure-functions/BinaryExpressions2.js
@@ -1,28 +1,19 @@
 // additional functions
 // abstract effects
 
-let obj1 = global.__abstract ? __abstract({}, '({foo: {valueOf() { return 42; }}})') : {foo: {valueOf() { return 42; }}};
-let obj2 = global.__abstract ? __abstract({}, '({foo: {bar: {valueOf() { return 42; }}}})') : {foo: {bar: {valueOf() { return 42; }}}};
-
-if (global.__makePartial) {
-  __makePartial(obj1);
-  __makePartial(obj2);
-}
-if (global.__makeSimple) {
-  __makeSimple(obj1);
-  __makeSimple(obj2);
-}
+let obj1 = global.__abstract ? __abstract('object', '({foo: {valueOf() { return 42; }}})') : {foo: {valueOf() { return 42; }}};
+let obj2 = global.__abstract ? __abstract('object', '({foo: {bar: {valueOf() { return 42; }}}})') : {foo: {bar: {valueOf() { return 42; }}}};
 
 function additional1() {
   return 42 < obj1.foo;
 }
 
 function additional2() {
-  return 42 < obj2.foo.bar;
+  return obj2.foo.bar > 42;
 }
 
 inspect = function() {
   let ret1 = additional1();
   let ret2 = additional2();
-  return ret1 + ret2;
+  return JSON.stringify({ ret1, ret2 });
 }
