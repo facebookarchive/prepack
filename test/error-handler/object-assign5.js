708aa8830ccfaf7d217f74669874faa0c26705c6
Dan Abramov <dan.abramov@gmail.com>
2018-02-21 14:49:07 -0800

    Change PP0023 error code to PP0026
    Summary:
    I just realized [PP0023 is already taken](https://github.com/facebook/prepack/wiki/PP0023).
    
    I couldn't find it by code search but even if it was removed, we still shouldn't reuse the same error code for a different problem.
    Closes https://github.com/facebook/prepack/pull/1479
    
    Reviewed By: trueadm
    
    Differential Revision: D7048029
    
    Pulled By: gaearon
    
    fbshipit-source-id: 74627233da069370722683b8c4295db3e913049c

--- test/error-handler/object-assign5.js
+++ test/error-handler/object-assign5.js
@@ -1,5 +1,5 @@
 // recover-from-errors
-// expected errors: [{"location":{"start":{"line":9,"column":14},"end":{"line":9,"column":16},"source":"test/error-handler/object-assign5.js"},"severity":"FatalError","errorCode":"PP0023"}]
+// expected errors: [{"location":{"start":{"line":9,"column":14},"end":{"line":9,"column":16},"source":"test/error-handler/object-assign5.js"},"severity":"FatalError","errorCode":"PP0026"}]
 
 var obj = global.__abstract && global.__makePartial && global.__makeSimple ? __makeSimple(__makePartial(__abstract({}, "({foo:1})"))) : {foo:1};
 
