b87d160517848e82381ab0f8a4ca58fe5ac541a9
Sebastian Markbage <sema@fb.com>
2018-03-12 11:57:38 -0700

    Don't visit a "widened property" value on a path
    Summary:
    This accidentally checked the pathNode on the key instead of the value.
    
    This is ignored by the serializer and the visitor needs to line up:
    
    https://github.com/facebook/prepack/blob/21fcfb2a0996abd2b90b61fb3ebd80bd3c9b8928/src/serializer/ResidualHeapSerializer.js#L261
    
    Fixes #1551
    Closes https://github.com/facebook/prepack/pull/1566
    
    Differential Revision: D7240690
    
    Pulled By: sebmarkbage
    
    fbshipit-source-id: db8c88c4dc7b9c3f87004435866cc87822ee2e81

--- src/serializer/ResidualHeapVisitor.js
+++ src/serializer/ResidualHeapVisitor.js
@@ -205,7 +205,7 @@ export class ResidualHeapVisitor {
       ) {
         continue;
       }
-      if (propertyBindingKey.pathNode !== undefined) continue; // property is written to inside a loop
+      if (propertyBindingValue.pathNode !== undefined) continue; // property is written to inside a loop
       invariant(propertyBindingValue);
       this.visitObjectProperty(propertyBindingValue);
     }
