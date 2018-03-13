00bfcde8b04086ed915dadc4080eb3782486ffdc
Dominic Gannaway <dg@domgan.com>
2018-03-13 15:08:31 +0000

    Failing tests

---
+++ test/serializer/additional-functions/conditions.js
@@ -0,0 +1,76 @@
+// abstract effects
+
+function getURL(getProtocol, getIsGeneric, getPort, getPath, serialize, getQueryData, getFragment, getForceFragmentSeparator) {
+  var str = "";
+  var protocol = getProtocol();
+  if (protocol) {
+    str += protocol + ":" + (getIsGeneric() ? "" : "//");
+  }
+  var domain = getDomain();
+  if (domain) {
+    str += domain;
+  }
+  var port = getPort();
+  if (port) {
+    str += ":" + port;
+  }
+
+  var path = getPath();
+  if (path) {
+    str += path;
+  } else if (str) {
+    str += "/";
+  }
+  var queryStr = serialize(
+    getQueryData()
+  );
+  if (queryStr) {
+    str += "?" + queryStr;
+  }
+  var fragment = getFragment();
+  if (fragment) {
+    str += "#" + fragment;
+  } else if (getForceFragmentSeparator()) {
+    str += "#";
+  }
+  return str;
+}
+
+__optimize(getURL);
+
+inspect = function inspect() {
+  var getProtocol = function() {
+    return "http://";
+  }
+  var getIsGeneric = function() {
+    return true;
+  }
+  var getPort = function() {
+    return 80;
+  }
+  var getPath = function() {
+    return "test";
+  }
+  var serialize = function(a) {
+    return a;
+  }
+  var getQueryData = function() {
+    return "test";
+  }
+  var getFragment = function() {
+    return "frag"
+  }
+  var getForceFragmentSeparator = function() {
+    return false;
+  }
+  return getURL(
+    getProtocol,
+    getIsGeneric,
+    getPort,
+    getPath,
+    serialize,
+    getQueryData,
+    getFragment,
+    getForceFragmentSeparator
+  );
+}
