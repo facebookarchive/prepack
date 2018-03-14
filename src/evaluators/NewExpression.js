77cb21510ff9eb2cf27cf2ad8b7d564d91ce4bf5
Dominic Gannaway <dg@domgan.com>
2018-03-14 12:39:31 +0000

    Provde a pure recovert for NewExpression

--- src/evaluators/NewExpression.js
+++ src/evaluators/NewExpression.js
@@ -11,20 +11,23 @@
 
 import type { Realm } from "../realm.js";
 import type { LexicalEnvironment } from "../environment.js";
-import { ObjectValue } from "../values/index.js";
+import { AbruptCompletion, PossiblyNormalCompletion } from "../completions.js";
+import { TypesDomain, ValuesDomain } from "../domains/index.js";
+import { ObjectValue, Value, AbstractObjectValue, AbstractValue } from "../values/index.js";
 import { Environment } from "../singletons.js";
-import { IsConstructor } from "../methods/index.js";
-import { ArgumentListEvaluation } from "../methods/index.js";
+import { IsConstructor, ArgumentListEvaluation } from "../methods/index.js";
 import { Construct } from "../methods/index.js";
 import invariant from "../invariant.js";
-import type { BabelNodeNewExpression } from "babel-types";
+import { FatalError } from "../errors.js";
+import * as t from "babel-types";
+import { BabelNodeNewExpression, type BabelNodeExpression } from "babel-types";
 
 export default function(
   ast: BabelNodeNewExpression,
   strictCode: boolean,
   env: LexicalEnvironment,
   realm: Realm
-): ObjectValue {
+): ObjectValue | AbstractObjectValue {
   realm.setNextExecutionContextLocation(ast.loc);
 
   // ECMA262 12.3.3.1 We just implement this method inline since it's only called here.
@@ -59,6 +62,70 @@ export default function(
     // b. ReturnIfAbrupt(argList).
   }
 
+  // If we are in pure scope, attempt to recover from creating the construct if
+  // it fails by creating a temporal abstract
+  if (realm.isInPureScope()) {
+    return tryToEvaluateConstructOrLeaveAsAbstract(constructor, argsList, strictCode, realm);
+  } else {
+    return createConstruct(constructor, argsList, realm);
+  }
+}
+
+function tryToEvaluateConstructOrLeaveAsAbstract(
+  constructor: Value,
+  argsList: Array<Value>,
+  strictCode: boolean,
+  realm: Realm
+): ObjectValue | AbstractObjectValue {
+  let effects;
+  try {
+    effects = realm.evaluateForEffects(
+      () => createConstruct(constructor, argsList, realm),
+      undefined,
+      "tryToEvaluateConstructOrLeaveAsAbstract"
+    );
+  } catch (error) {
+    // if the constructor is abstract, then create a unary op for it,
+    // otherwise we rethrow the error as we don't handle it at this
+    // point in time
+    if (error instanceof FatalError && constructor instanceof AbstractValue) {
+      let abstractValue = realm.evaluateWithPossibleThrowCompletion(
+        () =>
+          AbstractValue.createTemporalFromBuildFunction(
+            realm,
+            ObjectValue,
+            [constructor, ...argsList],
+            ([constructorNode, ...argListNodes]: Array<BabelNodeExpression>) =>
+              t.newExpression(constructorNode, argListNodes)
+          ),
+        TypesDomain.topVal,
+        ValuesDomain.topVal
+      );
+      invariant(abstractValue instanceof AbstractObjectValue);
+      return abstractValue;
+    } else {
+      throw error;
+    }
+  }
+  let completion = effects[0];
+  if (completion instanceof PossiblyNormalCompletion) {
+    // in this case one of the branches may complete abruptly, which means that
+    // not all control flow branches join into one flow at this point.
+    // Consequently we have to continue tracking changes until the point where
+    // all the branches come together into one.
+    completion = realm.composeWithSavedCompletion(completion);
+  }
+
+  // Note that the effects of (non joining) abrupt branches are not included
+  // in joinedEffects, but are tracked separately inside completion.
+  realm.applyEffects(effects);
+  // return or throw completion
+  if (completion instanceof AbruptCompletion) throw completion;
+  invariant(completion instanceof ObjectValue || completion instanceof AbstractObjectValue);
+  return completion;
+}
+
+function createConstruct(constructor: Value, argsList: Array<Value>, realm: Realm): ObjectValue {
   // 7. If IsConstructor(constructor) is false, throw a TypeError exception.
   if (IsConstructor(realm, constructor) === false) {
     throw realm.createErrorThrowCompletion(realm.intrinsics.TypeError);
