1ca1574b714fdfcab6a2ba01770ef2330823db55
Nikolai Tillmann <nikolait@fb.com>
2018-03-20 13:12:48 -0700

    Removing test comments // abstract effects

--- test/serializer/abstract/ObjectAssign15.js
+++ test/serializer/abstract/ObjectAssign15.js
@@ -1,5 +1,3 @@
-// abstract effects
-
 var __evaluatePureFunction = this.__evaluatePureFunction || (f => f());
 var obj = global.__abstract && global.__makePartial && global.__makeSimple ? __makeSimple(__makePartial(__abstract({foo: __abstract('number')}, "({foo:1})"))) : {foo:1};
 var copyOfObj;
@@ -17,6 +15,6 @@ __evaluatePureFunction(() => {
   });
 });
 
-inspect = function() {  
+inspect = function() {
   return JSON.stringify(y);
 }
--- test/serializer/abstract/ObjectAssign8.js
+++ test/serializer/abstract/ObjectAssign8.js
@@ -1,5 +1,3 @@
-// abstract effects
-
 var __evaluatePureFunction = this.__evaluatePureFunction || (f => f());
 var obj = global.__abstract && global.__makePartial && global.__makeSimple ? __makeSimple(__makePartial(__abstract({}, "({foo:1})"))) : {foo:1};
 var x;
@@ -11,6 +9,6 @@ __evaluatePureFunction(() => {
   y.foo = 2;
 });
 
-inspect = function() {  
+inspect = function() {
   return JSON.stringify(x);
 }
