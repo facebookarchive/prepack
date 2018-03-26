517fe539a893db4edde721b695cca8dab8a626f3
Dominic Gannaway <dg@domgan.com>
2018-03-26 12:55:53 +0100

    fix test syntax error

--- test/serializer/pure-functions/AbstractComputedProperty.js
+++ test/serializer/pure-functions/AbstractComputedProperty.js
@@ -4,9 +4,13 @@ if (!global.__evaluatePureFunction) {
 }
 
 __evaluatePureFunction(() => {
-  var x = global.__abstract ? __abstract(undefined, "({foo: 123})"): {foo: 123} ||
-    global.__abstract ? __abstract(undefind, "(false)") : false;
-  var y = global.__abstract ? __abstract(undefined, "foo") : "foo";
+  var x = (global.__abstract ? __abstract(undefined, "({foo: 123})"): {foo: 123}) ||
+    (global.__abstract ? __abstract(undefind, "(false)") : false);
+  var y = global.__abstract ? __abstract(undefined, "('foo')") : "foo";
 
   global.x = x[y];
 });
+
+inspect = function() {
+  return global.x;
+}
