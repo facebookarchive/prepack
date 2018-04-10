673e944ef30150c566b8a2335a490537d6c19337
Dominic Gannaway <dg@domgan.com>
2018-04-10 16:08:50 +0100

    use alternative test method

--- test/react/mocks/fb13.js
+++ test/react/mocks/fb13.js
@@ -8,14 +8,8 @@ if (window.__optimize) {
   __optimize(func);
 }
 
-// when running this test through the JS evaluator
-// rather than Prepack, window.__abstract will be
-// undefined, allowing the evaluation to run this
-// assertion
-if (!window.__abstract) {
-  if (!func.toString().includes(`Bootloader.loadModules(['Foo'], function () {}, "Bar");`)) {
-    throw new Error("Test failed!");
-  }
-}
+func.getTrials = function() {
+  return func.toString().includes(`Bootloader.loadModules(['Foo'], function () {}, "Bar");`);
+};
 
-window.result = func;
+module.exports = func;
