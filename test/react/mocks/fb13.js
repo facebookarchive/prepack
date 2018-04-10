91a85c6921925b54ba0a6a796512b66de74bdf6a
Dominic Gannaway <dg@domgan.com>
2018-04-10 16:00:47 +0100

    added comment

--- test/react/mocks/fb13.js
+++ test/react/mocks/fb13.js
@@ -9,6 +9,10 @@ if (window.__optimize) {
   __optimize(func);
 }
 
+// when running this test through the JS evaluator
+// rather than Prepack, window.__abstract will be
+// undefined, allowing the evaluation to run this
+// assertion
 if (!window.__abstract) {
   if (!func.toString().includes(`Bootloader.loadModules(['Foo'], function () {}, "Bar");`)) {
     throw new Error("Test failed!");
