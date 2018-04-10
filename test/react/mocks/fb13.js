9d6f9b2ec99435a32b3ce6ade3c0b53820c3258f
Dominic Gannaway <dg@domgan.com>
2018-04-10 16:21:26 +0100

    fix test

--- test/react/mocks/fb13.js
+++ test/react/mocks/fb13.js
@@ -12,6 +12,7 @@ func.getTrials = function(_, fn) {
   if (!fn.toString().includes('Bootloader.loadModules(')) {
     throw new Error('Expected to find Bootloader.loadModules() call.');
   }
+  return [];
 };
 
 module.exports = func;
