d516c4205e8f6b144d13cbf3d9a535deb7c4bdc1
Sapan Bhatia <gwsapan@fb.com>
2018-05-04 17:14:04 -0400

    Fix duplication in the face of conditional returns
    Resolves #1869

---
+++ test/serializer/abstract/Return10.js
@@ -0,0 +1,12 @@
+// count of 42:1
+
+(function () {
+    x = global.__abstract?__abstract("number", "5"):5;
+    if (x==5) {
+	    console.log("42");
+    } else  {
+	    return;
+    }
+})()
+
+let inspect = () => logstream;
