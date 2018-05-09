3d0a15b3f0e01ebbfc68245e2a163d21d7b00c83
Herman Venter <hermanv@fb.com>
2018-05-09 13:49:53 -0700

    Prevent CSE from hoisting Object.assign calls.

---
+++ test/serializer/optimized-functions/PropertyDeref.js
@@ -0,0 +1,14 @@
+function fn(arg) {
+  return arg != null && arg.x && arg.y; // needs both conjunctions to repro. Happens because of scope promotion
+};
+
+if (global.__optimize) __optimize(fn);
+
+inspect = function() {
+  return JSON.stringify([
+    fn(null),
+    // fn(undefined),
+    // fn({x: false, y: 5}),
+    // fn({x: 5, y: 10}),
+  ])
+}
