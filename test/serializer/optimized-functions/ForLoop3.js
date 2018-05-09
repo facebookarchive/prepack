c4c1593b22d7a75e48ca12c9f48c0c2f1e7abdc9
Dan Abramov <dan.abramov@gmail.com>
2018-05-09 09:39:28 -0700

    Add a serializer regression test for #1833
    Summary:
    Regression test.
    Closes https://github.com/facebook/prepack/pull/1916
    
    Differential Revision: D7931571
    
    Pulled By: gaearon
    
    fbshipit-source-id: a442ad2cb68c97d731dd8fbe03886562c8dde18c

---
+++ test/serializer/optimized-functions/ForLoop3.js
@@ -0,0 +1,22 @@
+(function() {
+  function fn(arg) {
+    if (arg.foo()) {
+      var arr = [0];
+      for (var k = 0; k < arr.length; ++k) {
+        break;
+      }
+    }
+    throw new Error("no");
+  }
+
+  if (global.__optimize) __optimize(fn);
+
+  global.inspect = function() {
+    try {
+      fn({foo() {}});
+    } catch (err) {
+      return err.message;
+    }
+    return 'expected an error';
+  }
+})();
