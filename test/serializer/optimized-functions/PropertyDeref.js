33941b228cec85e63a09542ffc1722330e6bd151
Herman Venter <hermanv@fb.com>
2018-05-09 14:02:21 -0700

    Update PropertyDeref.js

--- test/serializer/optimized-functions/PropertyDeref.js
+++ test/serializer/optimized-functions/PropertyDeref.js
@@ -7,8 +7,8 @@ if (global.__optimize) __optimize(fn);
 inspect = function() {
   return JSON.stringify([
     fn(null),
-    // fn(undefined),
-    // fn({x: false, y: 5}),
-    // fn({x: 5, y: 10}),
+    fn(undefined),
+    fn({x: false, y: 5}),
+    fn({x: 5, y: 10}),
   ])
 }
