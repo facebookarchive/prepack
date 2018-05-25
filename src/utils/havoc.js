afa6f69d25ce1cdc7f4f3e72f96bc6407dc6f93c
Dominic Gannaway <dg@domgan.com>
2018-05-25 23:59:46 +0100

    revert havoc.js

--- src/utils/havoc.js
+++ src/utils/havoc.js
@@ -397,7 +397,6 @@ class ObjectValueHavocingVisitor {
       // All intrinsic values exist from the beginning of time (except unknown arrays)...
       // ...except for a few that come into existance as templates for abstract objects.
       if (val instanceof ArrayValue && ArrayValue.isIntrinsicAndHasWidenedNumericProperty(val)) {
-        debugger;
         if (this.mustVisit(val)) this.visitValueObject(val);
       } else {
         this.mustVisit(val);
