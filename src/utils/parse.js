bd9c03d769faabf4b24ffc8cc9e84c7ce9287ee1
Paulo Cunha <paulomrcunha@gmail.com>
2018-05-29 00:24:09 +0200

    make some utils file flow strict

--- src/utils/parse.js
+++ src/utils/parse.js
@@ -7,7 +7,7 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 
-/* @flow */
+/* @flow strict-local */
 
 import invariant from "../invariant.js";
 import type { SourceType } from "../types.js";
