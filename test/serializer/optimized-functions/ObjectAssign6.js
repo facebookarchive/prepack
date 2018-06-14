d60ac6ab5d96521628a0696c57faeec0fd0b0a30
Dominic Gannaway <dg@domgan.com>
2018-06-15 00:49:46 +0100

    added more tests

---
+++ test/serializer/optimized-functions/ObjectAssign6.js
@@ -0,0 +1,12 @@
+// does not contain:return 1
+
+function fn(abstract) {
+  var a = Object.assign({}, {x: 1}, abstract);
+  return a.x;
+}
+
+global.__optimize && __optimize(fn);
+
+inspect = function() {
+  return fn({x: 2});
+}
---
+++ test/serializer/optimized-functions/ObjectAssign7.js
@@ -0,0 +1,12 @@
+// does contain:return 25
+
+function fn(abstract) {
+  var a = Object.assign({}, {x: 1}, abstract, {x: 25});
+  return a.x;
+}
+
+global.__optimize && __optimize(fn);
+
+inspect = function() {
+  return fn({x: 2});
+}
