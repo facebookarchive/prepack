6e869a33beb65172b20fd6a1966a5923fde767c5
David Cai <davidcai@fb.com>
2018-06-20 16:13:41 -0700

    Fixed a DebugAdapter error check bug (#2144)
    Summary:
    Release notes: Reversed an !== undefined check so the check behaves as intended
    Closes https://github.com/facebook/prepack/pull/2144
    
    Differential Revision: D8553142
    
    Pulled By: caiismyname
    
    fbshipit-source-id: ca808323ac96d0d3a0b6496106bd04988293249a

--- src/debugger/adapter/DebugAdapter.js
+++ src/debugger/adapter/DebugAdapter.js
@@ -21,6 +21,7 @@ import type {
   PrepackLaunchArguments,
 } from "./../common/types.js";
 import { DebuggerConstants } from "./../common/DebuggerConstants.js";
+import { DebuggerError } from "./../common/DebuggerError.js";
 
 /* An implementation of an debugger adapter adhering to the VSCode Debug protocol
  * The adapter is responsible for communication between the UI and Prepack
@@ -349,8 +350,11 @@ class PrepackDebugSession extends DebugSession {
     // All responses that involve the Adapter Channel should only be invoked
     // after the channel has been created. If this ordering is perturbed,
     // there was likely a change in the protocol implementation by Nuclide.
-    if (this._adapterChannel !== undefined) {
-      throw Error(`Adapter Channel in Debugger is being used before it has been created. Caused by ${callingRequest}.`);
+    if (this._adapterChannel === undefined) {
+      throw new DebuggerError(
+        "Startup Error",
+        `Adapter Channel in Debugger is being used before it has been created. Caused by ${callingRequest}.`
+      );
     }
   }
 }
