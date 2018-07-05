7680e66658e888e3c184c11c19046b9e67af6d20
Dominic Gannaway <dg@domgan.com>
2018-07-06 00:06:17 +0100

    added more tests

---
+++ test/serializer/optimized-functions/DeadObjectAssign14.js
@@ -0,0 +1,20 @@
+// Copies of _8:3
+// _8 is the variable for Object.assign. See DeadObjectAssign4.js for
+// a larger explanation.
+
+function f(x, foo) {
+  var a = Object.assign({}, foo);
+  var b = Object.assign({}, a);
+  // b gets visited
+  var someVal = b;
+  if (x) {
+    // a gets visited
+    someVal = a;
+  }
+  // a should still exist
+  return someVal;
+}
+
+if (global.__optimize) __optimize(f);
+
+global.inspect = function() { return f(false, {a: 3}); }
---
+++ test/serializer/optimized-functions/DeadObjectAssign15.js
@@ -0,0 +1,23 @@
+// Copies of _7:3
+// _7 is the variable for Object.assign. See DeadObjectAssign4.js for
+// a larger explanation.
+
+function f(x, foo) {
+  var a = Object.assign({}, foo);
+  var b = Object.assign({}, a);
+  // b gets visited
+  var someVal = b;
+  if (x) {
+    // a gets visited
+    function foo() {
+      return a;
+    }
+    someVal = foo;
+  }
+  // a should still exist
+  return someVal;
+}
+
+if (global.__optimize) __optimize(f);
+
+global.inspect = function() { return f(false, {a: 3}); }
