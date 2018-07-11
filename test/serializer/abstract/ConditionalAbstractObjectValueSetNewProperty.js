e6e6b8a5ac5248adc1c63cfe78c3dca5de14b19c
Herman Venter <hermanv@fb.com>
2018-07-11 13:04:26 -0700

    Remove the now dead vestiges of ErasedAbruptCompletion

--- test/serializer/abstract/ConditionalAbstractObjectValueSetNewProperty.js
+++ test/serializer/abstract/ConditionalAbstractObjectValueSetNewProperty.js
@@ -1,15 +1,12 @@
-(function () {
+(function() {
   let c = global.__abstract ? __abstract("boolean", "(true)") : true;
-  var a = {x:1};
+  var a = { x: 1 };
   var b = {};
   var obj = c ? a : b;
   obj.x = 2;
   global.result = a.x;
   global.residualObject = b;
   inspect = function() {
-    return (
-      global.result + ' ' +
-      global.residualObject.x + ' ' +
-      global.residualObject.hasOwnProperty('x')
-    ); };
+    return global.result + " " + global.residualObject.x + " " + global.residualObject.hasOwnProperty("x");
+  };
 })();
