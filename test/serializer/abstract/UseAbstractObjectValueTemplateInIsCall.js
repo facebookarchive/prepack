afa0f12e52406620b755a4a935f837b2e0145d71
Nikolai Tillmann <nikolait@fb.com>
2018-07-17 03:03:43 -0700

    Addressing feedback.

--- test/serializer/abstract/UseAbstractObjectValueTemplateInIsCall.js
+++ test/serializer/abstract/UseAbstractObjectValueTemplateInIsCall.js
@@ -1,4 +1,6 @@
 let obj = global.__abstract ? __abstract({}, "obj") : {};
-let func = global.__abstract ? __abstract(function(){}, "func") : function(){};
-global.result = (typeof obj) + "/" + (typeof func);
-global.inspect = function() { return global.result; }
+let func = global.__abstract ? __abstract(function() {}, "func") : function() {};
+global.result = typeof obj + "/" + typeof func;
+global.inspect = function() {
+  return global.result;
+};
