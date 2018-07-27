2334d329d150a678e8ae80e69e539fc509f03a55
Sebastian Markbage <sema@fb.com>
2018-07-27 13:47:21 -0700

    Special case conditionals for $DefineOwnProperty, $SetPartial, $GetPartial and $Delete (#2329)
    Summary:
    Follow up to #2219
    
    This also includes two relevant bonus fixes: https://github.com/facebook/prepack/commit/2aa1ad35a42fd4fe293513a96e89a8e59f1b34bc and https://github.com/facebook/prepack/commit/e96ac8c0c183566be50a00268c17c98bf4d8e18f
    Pull Request resolved: https://github.com/facebook/prepack/pull/2329
    
    Differential Revision: D9033950
    
    Pulled By: sebmarkbage
    
    fbshipit-source-id: b28b608b7449f0bbd7781c7d6aab5717d638d904

--- src/values/ObjectValue.js
+++ src/values/ObjectValue.js
@@ -807,13 +807,21 @@ export default class ObjectValue extends ConcreteValue {
       }
       result = AbstractValue.createFromType(this.$Realm, Value, "sentinel member expression", [this, P]);
     } else {
-      result = AbstractValue.createTemporalFromBuildFunction(
-        this.$Realm,
-        Value,
-        [this, P],
-        createOperationDescriptor("OBJECT_GET_PARTIAL"),
-        { skipInvariant: true, isPure: true }
-      );
+      // This is simple and not partial. Any access that isn't covered by checking against
+      // all its properties, is covered by reading from the prototype.
+      if (this.$Prototype === this.$Realm.intrinsics.null) {
+        // If the prototype is null, then the fallback value is undefined.
+        result = this.$Realm.intrinsics.undefined;
+      } else {
+        // Otherwise, we read the value dynamically from the prototype chain.
+        result = AbstractValue.createTemporalFromBuildFunction(
+          this.$Realm,
+          Value,
+          [this.$Prototype, P],
+          createOperationDescriptor("OBJECT_GET_PARTIAL"),
+          { skipInvariant: true, isPure: true }
+        );
+      }
     }
 
     // Get a specialization of the join of all values written to the object
