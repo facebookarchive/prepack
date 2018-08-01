f7aaf5ab0a176df5e22a4cd95c8e5d3796522ed4
Nikolai Tillmann <nikolait@fb.com>
2018-08-01 14:11:29 -0700

    Fixing error handler tests.

--- test/error-handler/write-forin-conflict.js
+++ test/error-handler/write-forin-conflict.js
@@ -1,5 +1,5 @@
 // recover-from-errors
-// expected errors: [{"severity":"Warning","errorCode":"PP1007","callStack":"Error\n    "},{"location":{"start":{"line":9,"column":16},"end":{"line":9,"column":22},"identifierName":"global","source":"test/error-handler/write-forin-conflict.js"},"severity":"FatalError","errorCode":"PP1003"}]
+// expected errors: [{"severity":"Warning","errorCode":"PP1007","callStack":"Error\n    "},{"location":{"start":{"line":9,"column":16},"end":{"line":9,"column":22},"identifierName":"global","source":"test/error-handler/write-forin-conflict.js"},"severity":"RecoverableError","errorCode":"PP1003"}]
 
 function additional1() {
   global.a = { f: "foo" };
