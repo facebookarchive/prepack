469c3e3cc9f43e545679238261bc38ef55efb056
Nikolai Tillmann <nikolait@fb.com>
2018-08-01 12:34:48 -0700

    Remove some dead code from test runner. Simplify test runner by removing option that's only used once for no good reason.

--- test/serializer/basic/FactorifyVoid0.js
+++ test/serializer/basic/FactorifyVoid0.js
@@ -1,6 +1,5 @@
 // skip lazy objects
 // Copies of void 0:1
-// do not inline expressions
 // omit invariants
 (function() {
   a = { x: 1, y: undefined };
@@ -8,6 +7,9 @@
   c = { x: 1, y: undefined };
   d = { x: 1, y: undefined };
   e = { x: 1, y: undefined };
+  // let's make sure we have at least two references to each value, so that they don't get inlined.
+  dummy1 = [a, b, c, d, e];
+  dummy2 = [a, b, c, d, e];
   inspect = function() {
     global.a.x + global.b.x + global.c.x + global.d.x + global.e.x;
   };
