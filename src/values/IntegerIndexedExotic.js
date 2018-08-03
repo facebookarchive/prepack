9767030bc6586b6979ba2a2a7d23ab065575c27c
Dominic Gannaway <dg@domgan.com>
2018-08-03 14:18:09 -0700

    Follow up to comments in 2357 (#2381)
    Summary:
    Release notes: none
    
    This is a follow up PR that addresses the post-land comments made in the PR #2357 from sebmarkbage.
    Pull Request resolved: https://github.com/facebook/prepack/pull/2381
    
    Differential Revision: D9160458
    
    Pulled By: trueadm
    
    fbshipit-source-id: d4b4fd98d88c0d91d4b4a4f14caf5179d68eff91

--- src/values/IntegerIndexedExotic.js
+++ src/values/IntegerIndexedExotic.js
@@ -262,11 +262,12 @@ export default class IntegerIndexedExotic extends ObjectValue {
       keys.push(new StringValue(this.$Realm, To.ToString(this.$Realm, new NumberValue(this.$Realm, i))));
     }
 
+    let realm = this.$Realm;
     // 5. For each own property key P of O such that Type(P) is String and P is not an integer index, in ascending chronological order of property creation
-    let properties = O.getOwnPropertyKeysArray();
-    for (let key of properties.filter(x => !IsArrayIndex(this.$Realm, x))) {
+    let properties = Properties.GetOwnPropertyKeysArray(realm, O, false, false);
+    for (let key of properties.filter(x => !IsArrayIndex(realm, x))) {
       // i. Add P as the last element of keys.
-      keys.push(new StringValue(this.$Realm, key));
+      keys.push(new StringValue(realm, key));
     }
 
     // 6. For each own property key P of O such that Type(P) is Symbol, in ascending chronological order of property creation
