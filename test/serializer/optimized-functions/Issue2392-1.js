39eec40920d6d63b64644ef0baaf371cff91e01c
Nikolai Tillmann <nikolait@fb.com>
2018-08-07 13:00:24 -0700

    Running prettier-all

--- test/serializer/optimized-functions/Issue2392-1.js
+++ test/serializer/optimized-functions/Issue2392-1.js
@@ -1,15 +1,17 @@
 function fn(x, obj, cond, cond4) {
-    var arr = Array.from(x);
+  var arr = Array.from(x);
 
-    var f = function(item) { return cond4 ? a : 5; };
-    if (cond) {
-        var a = obj.a;
-        return arr.map(f);
-    }
+  var f = function(item) {
+    return cond4 ? a : 5;
+  };
+  if (cond) {
+    var a = obj.a;
+    return arr.map(f);
+  }
 }
 
 global.__optimize && __optimize(fn);
 
 global.inspect = function() {
-    return true;
-}
+  return true;
+};
--- test/serializer/optimized-functions/Issue2392-2.js
+++ test/serializer/optimized-functions/Issue2392-2.js
@@ -1,25 +1,25 @@
 function fn(x, obj, cond, cond2, cond3, cond4) {
-    var arr = Array.from(x);
-    var a;
-    var b;
-    var res;
+  var arr = Array.from(x);
+  var a;
+  var b;
+  var res;
 
-    if (cond) {
-        a = obj.a;
-        if (cond2) {
-            b = obj.b;
-            if (cond3) {
-                res = arr.map(function(item) {
-                        return cond4 ? a : b;
-                    });
-            }
-        }
+  if (cond) {
+    a = obj.a;
+    if (cond2) {
+      b = obj.b;
+      if (cond3) {
+        res = arr.map(function(item) {
+          return cond4 ? a : b;
+        });
+      }
     }
-    return res;
+  }
+  return res;
 }
 
 global.__optimize && __optimize(fn);
 
 global.inspect = function() {
-    return true;
-}
+  return true;
+};
