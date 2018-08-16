c0efe8f4f2eeba908a55a22fa9f5a82ded15f99c
Caleb Meredith <calebmeredith8@gmail.com>
2018-08-16 12:45:24 -0700

    use appropriate stack size for coverage test runner?

--- test/serializer/pure-functions/StackOverflow.js
+++ test/serializer/pure-functions/StackOverflow.js
@@ -8,7 +8,7 @@ global.__d = () => {};
 
 const result = global.__evaluatePureFunction(() => {
   function loop(i) {
-    if (i === 9999) {
+    if (i === 980) {
       return "Hello, world";
     }
     // Prevent tail recursion optimizations from applying.
