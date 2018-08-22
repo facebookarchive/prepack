c44461670124e05348019fb4ba00c04138257b18
Caleb Meredith <calebmeredith8@gmail.com>
2018-08-22 11:24:34 -0700

    remove unused variable

--- test/serializer/pure-functions/FatalErrorAfterModifiedBinding.js
+++ test/serializer/pure-functions/FatalErrorAfterModifiedBinding.js
@@ -1,7 +1,7 @@
 if (!global.__evaluatePureFunction) global.__evaluatePureFunction = f => f();
 
 const result = global.__evaluatePureFunction(() => {
-  let x, y;
+  let x;
 
   function f() {
     const getNumber = global.__abstract ? global.__abstract("function", "(() => 1)") : () => 1;
