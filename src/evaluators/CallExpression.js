dfff2056b761d6091ab5d73dfee89750b345c788
Caleb Meredith <calebmeredith8@gmail.com>
2018-08-23 15:12:30 -0700

    Leak function in call bailout (#2466)
    Summary:
    Fixes #2464.
    
    When we bail out of a function call because of a `FatalError` we did not leak the function we called. This means modified bindings were not appropriately leaked. See issue #2464 where the result is `undefined` instead of a reference to the leaked value.
    
    This PR also adds a utility `__fatal` function for throwing a fatal error. Allowing us to more clearly express issues involving `FatalError`s. Used in the test.
    Pull Request resolved: https://github.com/facebook/prepack/pull/2466
    
    Differential Revision: D9482612
    
    Pulled By: calebmer
    
    fbshipit-source-id: 792d29818245ad9f631227d923efb56228df21e0

--- src/evaluators/CallExpression.js
+++ src/evaluators/CallExpression.js
@@ -274,7 +274,9 @@ function tryToEvaluateCallOrLeaveAsAbstract(
     );
   } catch (error) {
     if (error instanceof FatalError) {
+      if (func instanceof NativeFunctionValue && func.name === "__fatal") throw error;
       realm.suppressDiagnostics = savedSuppressDiagnostics;
+      Leak.value(realm, func, ast.loc);
       return realm.evaluateWithPossibleThrowCompletion(
         () => generateRuntimeCall(ref, func, ast, strictCode, env, realm),
         TypesDomain.topVal,
