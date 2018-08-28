52d451384180039b0e11dddcb8d60f21424daf7d
Jonathan Wieben <joni.wieben@icloud.com>
2018-08-28 04:20:36 -0700

    Fix: Indentation in EliminationOfAbstractionTax example (#2496)
    Summary:
    Release Note: none
    
    Fixes indentation in REPL of EliminationOfAbstractionTax example.
    Pull Request resolved: https://github.com/facebook/prepack/pull/2496
    
    Differential Revision: D9528783
    
    Pulled By: trueadm
    
    fbshipit-source-id: 9c01f0783c6299d2577d7ac17ff2b39db9e5cca0

--- website/js/repl.js
+++ website/js/repl.js
@@ -282,7 +282,7 @@ function addDefaultExamples() {
   code = [
     '(function () {',
     '  var self = this;',
-    '    ["A", "B", 42].forEach(function(x) {',
+    '  ["A", "B", 42].forEach(function(x) {',
     '    var name = "_" + x.toString()[0].toLowerCase();',
     '    var y = parseInt(x);',
     '    self[name] = y ? y : x;',
