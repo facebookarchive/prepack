1e5a0c05009aacca5cd3d7856e61da9f3259c5dc
Chris Blappert <blappert@fb.com>
2018-09-21 13:27:16 -0700

    Prettier tests

--- test/serializer/optimized-functions/Issue2423.js
+++ test/serializer/optimized-functions/Issue2423.js
@@ -1,20 +1,22 @@
 // does not contain:23
 (function() {
-    function f(c) {
-        let h;
-        if (c) {
-            h = function() { return 23 + 42; };
-            function g() {
-                return h();
-            };
-            global.__optimize && __optimize(g);
-            return g;
-        }
+  function f(c) {
+    let h;
+    if (c) {
+      h = function() {
+        return 23 + 42;
+      };
+      function g() {
+        return h();
+      }
+      global.__optimize && __optimize(g);
+      return g;
     }
-    global.__optimize && __optimize(f);
-    global.f = f;
+  }
+  global.__optimize && __optimize(f);
+  global.f = f;
 
-		global.inspect = function() {
-				return f(true)();
-		}
+  global.inspect = function() {
+    return f(true)();
+  };
 })();
--- test/serializer/optimized-functions/WrongScopeTripleNested.js
+++ test/serializer/optimized-functions/WrongScopeTripleNested.js
@@ -1,16 +1,18 @@
-(function () {
-    function outer() {
-        let x = {};
-        function middle() {
-            function inner() { return x; }
-            if (global.__optimize) __optimize(inner);
-            return inner;
-        };
-        if (global.__optimize) __optimize(middle);
-        return middle;
+(function() {
+  function outer() {
+    let x = {};
+    function middle() {
+      function inner() {
+        return x;
+      }
+      if (global.__optimize) __optimize(inner);
+      return inner;
     }
-    if (global.__optimize) __optimize(outer);
-    global.inspect = function() {
-        return outer()()() === outer()()();
-    }
+    if (global.__optimize) __optimize(middle);
+    return middle;
+  }
+  if (global.__optimize) __optimize(outer);
+  global.inspect = function() {
+    return outer()()() === outer()()();
+  };
 })();
